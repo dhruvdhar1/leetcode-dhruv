fe06440dca6b6a8241b712d4b2d0a78723602f53
Dhruv Dhar <dhruvdhar1@gmail.com>
2022-10-13 23:53:32 -0400

    Time: 200 ms (88.27%), Space: 47.7 MB (97.96%) - LeetHub

--- 0952-largest-component-size-by-common-factor/0952-largest-component-size-by-common-factor.java
+++ 0952-largest-component-size-by-common-factor/0952-largest-component-size-by-common-factor.java
@@ -1,59 +1,50 @@
 class Solution {
-	private class UnionFind {
-		private int[] parent;
-
-		private UnionFind(int n){
-			parent = new int[n];
-			for(int i=0;i<n;i++){
-				parent[i] = i;
-			}
-		}
-		private int getAbsoluteParent(int i){
-			if(parent[i]==i){
-				// absolute parent
-				return i;
-			}
-			parent[i]=getAbsoluteParent(parent[i]);
-			return parent[i];
-		}
-
-		private void unify(int i, int j){
-			int absoluteParentI = getAbsoluteParent(i);
-			int absoluteParentJ = getAbsoluteParent(j);
-			if(absoluteParentI!=absoluteParentJ){
-				// 7->2, 21->2 (7,21)
-				parent[absoluteParentJ] = absoluteParentI;
-			}
-		}
-
-	}
-
-	public int largestComponentSize(int[] nums) {
-		int max=0;
-		for(int el:nums) {
-			max = Math.max(el, max);
-		}
-		UnionFind uf = new UnionFind(max+1);
-
-		for(int i:nums) {
-			for(int j=2;j*j<=i;j++) {
-				if(i%j==0){ // j is a factor of i
-					uf.unify( j,i); // i=21, j=3
-					uf.unify(i/j,i); // (21,3), (21,21/3(7))
-				}
-			}
-		}
-		//<Parent, Freq>
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    
+    private void union(int a, int b, int[] parent) {
+        int parentA = find(a, parent);
+        int parentB = find(b, parent);
+        
+        if(parentA != parentB) {
+            parent[parentA] = parentB;
+        }
+    }
+    
+    private int find(int a, int[] parent) {
+        while(a != parent[a]) {
+            a = parent[a];
+            parent[a] = parent[parent[a]];
+        }
+        return a;
+    }
+    
+    public int largestComponentSize(int[] nums) {
+        int max = 0;
+        for(int num: nums) {
+            max = Math.max(num, max);
+        }
+        
+        int[] parent = new int[max+1];
+        
+        for(int i=0; i<parent.length; i++) {
+            parent[i] = i;
+        }
+        
+        for(int i: nums) {
+            for(int j=2; j*j<=i; j++) {
+                if(i%j == 0) {
+                    union(j, i, parent);
+                    union(i/j, i, parent);
+                }
+            }
+        }
+        
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 		int maxComponet = 0;
 		for(int el:nums){
-			int parent = uf.getAbsoluteParent(el);
-			map.put(parent, map.getOrDefault(parent, 0) + 1);
-			maxComponet= Math.max(maxComponet, map.get(parent));
+			int p = find(el, parent);
+			map.put(p, map.getOrDefault(p, 0) + 1);
+			maxComponet= Math.max(maxComponet, map.get(p));
 		}
-
-
-
-		return maxComponet;
-	}
+        return maxComponet;
+    }
 }
