9f8d5f81ef72804ac82c80832dd46ca457c76afa
Dhruv Dhar <dhruvdhar1@gmail.com>
2022-10-21 01:33:54 -0400

    Time: 101 ms (41.60%), Space: 67.1 MB (66.81%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.java
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.java
@@ -1,18 +1,14 @@
 class Solution {
     private int backtrack(String s, int i, int j, Set<String> set) {
         if(j == s.length()) {
-            // System.out.println("return : " + set.size());
             return set.size();
         }
-        
         int max = -1;
         for(int k=j+1; k<=s.length(); k++) {
             String substr = s.substring(j, k);
             if(!set.contains(substr)) {
-                // System.out.println(j + ":" + k + " : " + substr);
                 set.add(substr);
                 int res = backtrack(s, j, k, set);
-                // System.out.println(substr + " : " + res);
                 max = Math.max(res, max);
                 set.remove(substr);
             }
