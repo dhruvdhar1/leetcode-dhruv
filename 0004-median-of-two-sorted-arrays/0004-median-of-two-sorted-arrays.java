0e9160d10d77bc8ba5522d71fab581677ebedc41
Dhruv Dhar <dhruvdhar1@gmail.com>
2022-10-23 14:09:42 -0400

    Time: 4 ms (80.17%), Space: 50 MB (38.13%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,32 +1,37 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] A = nums1.length <= nums2.length ? nums1 : nums2;
-        int[] B = nums1.length > nums2.length ? nums1 : nums2;
+        int l1 = nums1.length;
+        int l2 = nums2.length;
         
-        // System.out.println(Arrays.toString(A));
-        // System.out.println(Arrays.toString(B));
+        int[] A;
+        int[] B;
         
-        int len = nums1.length + nums2.length;
-        int half = len/2;
+        if(l1 < l2) {
+            A = nums1;
+            B = nums2;
+        } else {
+            A = nums2;
+            B = nums1;
+        }
+        l1 = A.length;
+        l2 = B.length;
         int l = 0, r = A.length;
-        
-        while(l<=r) {
-            int i = l + (r-l)/2;
-            int j = half-i;
+        while(l<= r) {
+            int i = l+(r-l)/2; //0
+            int j = ((l1+l2)/2) - i;
+            // System.out.println("j: " + j + " : " + l2);
+            int aL = (i==0) ? Integer.MIN_VALUE : A[i-1];
+            int bL = (j==0) ? Integer.MIN_VALUE : B[j-1];
+            int aR = (i==l1) ? Integer.MAX_VALUE : A[i];
+            int bR = (j==l2) ? Integer.MAX_VALUE : B[j];
             
-            double aLeft = i == 0 ? Integer.MIN_VALUE : A[i-1];
-            double bLeft = j == 0 ? Integer.MIN_VALUE : B[j-1];
-            double aRight = i == A.length ? Integer.MAX_VALUE : A[i];
-            double bRight = j == B.length ? Integer.MAX_VALUE : B[j];
-            
-            if(aLeft <= bRight && bLeft <= aRight) {
-                // System.out.println("ssss");
-                if(len%2 == 0) {
-                    return ((Math.max(aLeft, bLeft) + Math.min(aRight, bRight))/2);
+            if(aL <= bR && bL <= aR) {
+                if((l1+l2)% 2 == 0) {
+                    return (double)(Math.max(aL, bL) + Math.min(aR, bR))/2;
                 } else {
-                    return Math.min(aRight, bRight);
+                    return Math.min(aR, bR);
                 }
-            } else if(aLeft > bRight) {
+            } else if (aL > bR) {
                 r = i-1;
             } else {
                 l = i+1;
