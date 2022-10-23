e5658440b1b5748ca8d998ed5bc3a0bfd529348a
Dhruv Dhar <dhruvdhar1@gmail.com>
2022-10-23 12:21:56 -0400

    Time: 122 ms (8.47%), Space: 41.9 MB (65.93%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.java
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.java
@@ -1,22 +1,25 @@
 class Solution {
-    private int backtrack(int start, int end, int[][] dp) {
-        if(start >= end) return 0;
+    
+    private int backtrack(int start, int end, int cost, int[][] dp) {
+        if(start>=end) return 0;
         if(dp[start][end] != -1) return dp[start][end];
-        int res = Integer.MAX_VALUE;
+        
+        int min = Integer.MAX_VALUE;
         for(int i=start; i<=end; i++) {
-            res = Math.min(res, i+Math.max(backtrack(start, i-1, dp), backtrack(i+1, end, dp)));
+            // System.out.println("iiiiiii : "+i);
+            min = Math.min(min, i+Math.max(backtrack(start, i-1, cost+i, dp), backtrack(i+1, end, cost+i, dp)));
+            // System.out.println("minmmmmmm : "+min);
         }
-        dp[start][end] = res;
-        return res;
+        dp[start][end] = min;
+        return min;
     }
     public int getMoneyAmount(int n) {
-        // Map<String, Integer> dp = new HashMap();
         int[][] dp = new int[n+1][n+1];
-        for(int i=0; i<dp.length; i++) {
-            for(int j=0; j<dp[0].length; j++) {
+        for(int i=0; i<n+1;i++) {
+            for(int j=0; j<n+1;j++) {
                 dp[i][j] = -1;
             }
         }
-        return backtrack(1, n, dp);
+        return backtrack(1, n, 0, dp);
     }
 }
