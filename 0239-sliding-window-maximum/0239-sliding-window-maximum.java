770a5e282b99fbec5bc11a192db4fc3151942108
Dhruv Dhar <dhruvdhar1@gmail.com>
2022-10-27 21:35:06 -0400

    Time: 113 ms (17.70%), Space: 60.8 MB (88.11%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,40 @@
+class Solution {
+    class Element{
+        int el;
+        int ind;
+        Element(int el, int ind) {
+            this.el = el;
+            this.ind = ind;
+        }
+    }
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<Element> maxHeap = new PriorityQueue<>((e1, e2) -> e2.el - e1.el);
+        for(int i=0; i<k; i++) {
+            Element el = new Element(nums[i], i);
+            maxHeap.add(el);
+        }
+        List<Integer> max = new ArrayList();
+        int i=0;
+        int start = 0, end = k-1;
+        while(end < nums.length) {
+            if(start != 0) {
+                maxHeap.add(new Element(nums[end], end));
+            }
+            while(maxHeap.size() > 0 && maxHeap.peek().ind < start) {
+                maxHeap.poll();
+            }
+            if(maxHeap.size() > 0) {
+                max.add(maxHeap.peek().el);
+                // res[i++] = maxHeap.peek().el;
+            }
+            start++;
+            end++;
+        }
+        int[] res = new int[max.size()];
+        for(int m: max) {
+            res[i++] = m;
+        }
+        // System.out.println("res: " + max);
+        return res;
+    }
+}
