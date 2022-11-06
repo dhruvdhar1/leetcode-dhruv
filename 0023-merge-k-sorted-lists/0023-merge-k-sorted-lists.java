ce39efbdd1fe3fa9bbcbc7cbf03d48bb1bf70785
Dhruv Dhar <dhruvdhar1@gmail.com>
2022-11-06 01:47:43 -0400

    Time: 14 ms (28.76%), Space: 49 MB (5.52%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    class Pair {
+        int element;
+        int listInd;
+        Pair(int element, int listInd) {
+            this.element = element;
+            this.listInd = listInd;
+        }
+        public String toString() {
+            return element + " : " + listInd;
+        }
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Pair> minHeap = new PriorityQueue<Pair>(
+                                            (p1, p2) -> p1.element-p2.element);
+        ListNode res = new ListNode(-1);
+        ListNode resHead = res;
+        for(int i=0; i<lists.length; i++) {
+            ListNode root = lists[i];
+            if(null != root) {
+                Pair p = new Pair(root.val, i);
+                minHeap.add(p);
+            }
+        }
+        // System.out.println(minHeap.toString());
+        while(!minHeap.isEmpty()) {
+            Pair pop = minHeap.poll();
+            // System.out.println("------- " + pop.toString());
+            res.next = new ListNode(pop.element);
+            res = res.next;
+            lists[pop.listInd] = lists[pop.listInd].next;
+            if(lists[pop.listInd] != null) {
+                Pair p = new Pair(lists[pop.listInd].val, pop.listInd);
+                minHeap.add(p);
+            }
+        }
+        return resHead.next;
+    }
+}
