22fb545c64b8fcdc38803a85e265d361391d0c09
Dhruv Dhar <dhruvdhar1@gmail.com>
2022-11-10 02:13:49 -0500

    Time: 9 ms (56.79%), Space: 44.7 MB (8.34%) - LeetHub

--- 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
@@ -1,13 +1,10 @@
 class Solution {
     public boolean isValidSerialization(String preorder) {
         String[] val = preorder.split(",");
-        System.out.println("++++ " + Arrays.toString(val));
         int len = val.length;
         int node = 0;
         int l = -1;
         int r = 0;
-        
-        //check that ind 0 is not #
         if(r >= len) return false;
         
         while(node < len) {
@@ -16,13 +13,10 @@ class Solution {
                 node++;
                 continue;
             }
-            // if(node != 0) {
             l = r+1;
             r = r+2;
-            // }
             
             if(l >= len || r >= len) {
-                // System.out.println("---- " + node + " : " + l + " : " + r);
                 return false;
             }
             node++;
