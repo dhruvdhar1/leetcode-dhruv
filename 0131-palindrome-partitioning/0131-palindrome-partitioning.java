3a18eba8240d6ebd220118a12bc891f64acbb39e
Dhruv Dhar <dhruvdhar1@gmail.com>
2022-11-10 22:43:46 -0500

    Time: 19 ms (76.90%), Space: 55.4 MB (86.38%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -2,25 +2,21 @@ class Solution {
     private boolean isPalindrome(String str) {
         StringBuilder sb = new StringBuilder(str);
         boolean res = sb.toString().equals(sb.reverse().toString());
-        // System.out.println("-------palindrome " + str + " : " + res);
         return res;
     }
-    private void backtrack(String s, int ind, String curr, List<String> set, List<List<String>> res) {
+    private void backtrack(String s, int ind, List<String> set, List<List<String>> res) {
         
         if(ind == s.length()) {
             List<String> copy = new ArrayList(set);
-            // System.out.println(">>>>>> " + copy);
             res.add(copy);
             return;
         }
         
         for(int i=ind+1; i<=s.length(); i++) {
             String subStr = s.substring(ind, i);
-            // System.out.println("-------substr " + subStr);
             if(isPalindrome(subStr)) {
                 set.add(subStr);
-                // System.out.println("-------set " + set.toString());
-                backtrack(s, i, subStr, set, res);
+                backtrack(s, i, set, res);
                 set.remove(set.size()-1);
             }
         }
@@ -29,7 +25,7 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> res = new ArrayList();
         List<String> set = new ArrayList();
-        backtrack(s, 0, "", set, res);
+        backtrack(s, 0, set, res);
         return res;
     }
 }
