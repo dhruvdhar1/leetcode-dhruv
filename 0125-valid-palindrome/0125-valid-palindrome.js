3a70978ab7b8747545db8901fcc6961ea846088c
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-06-22 21:05:31 -0400

    Time: 247 ms (5.04%), Space: 57.7 MB (9.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,14 +3,26 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    const charArr = []
-    for(let i=0; i<s.length; i++) {
-        const ch = s[i]
-        if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
-            charArr.push(ch.toLowerCase())
+    let l=0, r=s.length-1
+
+    while(l<r) {
+        const lch = s[l]
+        const rch = s[r]
+
+        console.log("Lch: ", lch, "rch: ", rch)
+        if(!((lch >= 'a' && lch <= 'z') || (lch >= 'A' && lch <= 'Z') || (lch >= '0' && lch <= '9'))) {
+
+            l++
+            continue
         }
+        if(!((rch >= 'a' && rch <= 'z') || (rch >= 'A' && rch <= 'Z') || (rch >= '0' && rch <= '9'))) {
+
+            r--
+            continue
+        }
+        if(lch.toLowerCase() !== rch.toLowerCase()) return false
+        l++
+        r--
     }
-    const str = charArr.join('')
-    const rev = charArr.reverse().join('')
-    return str === rev
+    return true
 };
