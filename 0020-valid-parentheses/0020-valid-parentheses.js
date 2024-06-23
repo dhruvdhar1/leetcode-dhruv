8fa95c16a3de4f0f4278f070befcde1bbab9dc2b
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-06-22 20:43:43 -0400

    Time: 113 ms (5.02%), Space: 55.6 MB (5.28%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = []
+
+    for(let i=0; i<s.length; i++) {
+        const ch = s[i]
+        console.log("ch: ", ch)
+        if(ch === ')' || ch === '}' || ch ===']') {
+            const pop = stack.pop()
+            if(ch === ')' && pop !== '(') {
+                return false
+            } else if(ch === '}' && pop !== '{') {
+                return false
+            } else if(ch === ']' && pop !== '[') {
+                return false
+            }
+        } else {
+            stack.push(ch)
+        }
+    }
+    return stack.length === 0
+};
