097f09ab3c74894a940a0c3eb784e06b1fcff56e
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-06-24 19:27:26 -0400

    Time: 48 ms (78.98%), Space: 48.9 MB (72.4%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -8,24 +8,14 @@ var searchInsert = function(nums, target) {
 
     while(l<r) {
         const mid = l + Number.parseInt((r-l)/2)
-        console.log("mid: ", mid)
         if(nums[mid] === target) return mid
         else if(nums[mid] > target) {
-            r = mid-1
+            r = mid
         } else {
             l = mid+1
         }
     }
-    if(nums[l] === target) return l
-    if(l === nums.length-1) {
-        if(target > nums[nums.length-1]) return l+1
-        else return l
-    }
-    if(l === 0) {
-        if(target < nums[0]) return 0
-        else return 1
-    }
-    if(nums[l] > target) return l
-    else if(nums[l] < target) return l+1
+    if(nums[l] < target) return l+1
+    else return l
 
 };
