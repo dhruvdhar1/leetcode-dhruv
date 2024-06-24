b08d3a598bab49d7c7a0c92a7113dd30b1f63d22
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-06-23 22:14:23 -0400

    Time: 56 ms (82%), Space: 50.1 MB (87.35%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -14,15 +14,5 @@ var isIsomorphic = function(s, t) {
             map.set(ch1, ch2)
         }
     }
-    map = new Map()
-    for(let i=0; i<s.length; i++) {
-        const ch1 = s[i]
-        const ch2 = t[i]
-        if(map.has(ch2)) {
-            if(ch1 !== map.get(ch2)) return false
-        } else {
-            map.set(ch2, ch1)
-        }
-    }
-    return true
+    return new Set([...map.values()]).size === map.size
 };
