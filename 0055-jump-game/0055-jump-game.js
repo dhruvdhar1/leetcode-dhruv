f13dcd75e3c31b69cb6e321cca36999113a4dbb4
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-06-24 23:38:15 -0400

    Time: 77 ms (23.2%), Space: 52.4 MB (94.07%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -3,15 +3,12 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    let l=0, r=0
-    while(r < nums.length-1) {
-        let farthest = 0
-        for(let i=l; i<r+1; i++) {
-            farthest = Math.max(farthest, i+nums[i])
+    let target = nums.length-1
+
+    for(let i=nums.length-2; i>=0; i--) {
+        if(i+nums[i] >= target) {
+            target = i
         }
-        l = r+1
-        r = farthest
-        if(r<l) return false
     }
-    return true
+    return target === 0
 };
