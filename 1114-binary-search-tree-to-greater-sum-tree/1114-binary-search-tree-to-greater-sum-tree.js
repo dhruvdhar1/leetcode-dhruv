1ee376230f2169a5083b62113b740eb05a57546d
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-06-25 22:53:40 -0400

    Time: 59 ms (23.89%), Space: 48.8 MB (89.42%) - LeetHub

---
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function(root) {
+    let currSum = 0
+    const solve = (node) => {
+        if(node == null) return
+
+        solve(node.right)
+        const ogValue = node.val
+        node.val += currSum
+        currSum += ogValue
+        solve(node.left)
+    }
+
+    solve(root)
+    return root
+};
