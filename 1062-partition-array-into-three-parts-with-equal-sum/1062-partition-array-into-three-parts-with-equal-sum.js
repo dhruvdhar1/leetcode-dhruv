2d86241cf27bbef954219eb8f07407b25d2fb42f
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-15 12:33:54 -0400

    Time: 59 ms (74.64%), Space: 52.8 MB (68.12%) - LeetHub

---
+++ 1062-partition-array-into-three-parts-with-equal-sum/1062-partition-array-into-three-parts-with-equal-sum.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canThreePartsEqualSum = function(arr) {
+    let sum = arr.reduce((acc, curr) => {
+        acc += curr
+        return acc
+    }, 0)
+    // console.log("----sum: ", sum)
+    if(sum%3 !== 0) return false
+
+    let count = 0
+    const partSize = sum/3
+    let currSum = 0
+    for(let i=0; i<arr.length; i++) {
+        currSum += arr[i]
+        if(currSum === partSize) {
+            currSum = 0
+            count++
+        }
+    }
+    console.log("----count: ", count)
+    return count >= 3
+};
