02c590d078d508113fb4c61177c436b0d67b9fd8
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-15 15:29:19 -0400

    Time: 74 ms (49.5%), Space: 55.5 MB (46.75%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -3,20 +3,21 @@
  * @return {boolean}
  */
 var validPalindrome = function(s) {
-    const dp = new Map()
-    const backtrack = (s, p, q, deleted) => {
-        if(q <= p) return true
-        const key = `${p}:${q}`
-        if(dp.has(key)) return dp.get(key)
-        let res = false
-        if(s[p] === s[q]) {
-            res = backtrack(s, p+1, q-1, deleted)
-        } else {
-            if(deleted) return false
-            res = backtrack(s, p+1, q, true) || backtrack(s, p, q-1, true)
+    let l = 0, r = s.length-1
+    while(l < r) {
+        if(s[l] !== s[r]) {
+            return isPalindrome(s, l+1, r) || isPalindrome(s, l, r-1)
         }
-        dp.set(key, res)
-        return res
+        l++
+        r--
     }
-    return backtrack(s, 0, s.length-1, false)
+    return true
 };
+const isPalindrome = (s, l, r) => {
+    while(l < r) {
+        if(s[l] !== s[r]) return false
+        l++
+        r--
+    }
+    return true
+}
