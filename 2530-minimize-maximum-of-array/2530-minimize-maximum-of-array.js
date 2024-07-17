327676bc74ac0a426fea564dc432c8d81a1c1db3
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-17 14:00:26 -0400

    Time: 69 ms (66.67%), Space: 59.6 MB (25.93%) - LeetHub

---
+++ 2530-minimize-maximum-of-array/2530-minimize-maximum-of-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimizeArrayValue = function(nums) {
+    let res = nums[0]
+    let sum = nums[0]
+    for(let i=1; i<nums.length; i++) {
+        sum += nums[i]
+        const max = Math.ceil(sum/(i+1))
+        if(max > res) res = max
+    }
+    return res
+};
