aeb907ee8780adb5d3128f58dd21ae10d431c85b
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-17 00:23:51 -0400

    Time: 288 ms (6.38%), Space: 68.2 MB (6.38%) - LeetHub

---
+++ 2351-number-of-ways-to-buy-pens-and-pencils/2351-number-of-ways-to-buy-pens-and-pencils.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} total
+ * @param {number} cost1
+ * @param {number} cost2
+ * @return {number}
+ */
+var waysToBuyPensPencils = function(total, cost1, cost2) {
+    let numPen = 0
+    let count = 0
+    while(numPen * cost1 <= total) {
+        const rem = total - numPen*cost1
+        count += (parseInt(rem/cost2) + 1)
+        numPen++
+    }
+    return count
+};
