147702c2318c10e0a49280c7389227725b13b251
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-18 17:36:05 -0400

    Time: 86 ms (69.46%), Space: 53.5 MB (82.49%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.js
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -5,15 +5,17 @@
  */
 var convert = function(s, numRows) {
     if(numRows === 1) return s
-    let res = ""
-    for(let r=0; r<numRows; r++) {
-        const increment = 2 * (numRows-1)
-        for(let i=r; i<s.length; i+=increment) {
-            res += s[i]
-            if(r > 0 && r < numRows - 1 && ((i + increment - 2*r) < s.length)) {
-                res += s[i + increment - 2*r]
+    
+    let str = ""
+    for(let i=0; i<numRows; i++) {
+        const offset = 2 * (numRows-1)
+        for(let j = i; j<s.length; j= j+offset) {
+            str += s[j]
+            if(i > 0 && i < numRows-1 && (j + offset - 2*i) < s.length) {
+                // console.log("-- ", s[j + offset - 2*i])
+                str += s[j + offset - 2*i]
             }
         }
     }
-    return res
+    return str
 };
