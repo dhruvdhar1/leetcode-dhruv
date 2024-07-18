273aa131a9b8b7cb889d71ffd8edfc77324ce663
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-17 20:17:07 -0400

    Time: 99 ms (10.63%), Space: 56.1 MB (12.2%) - LeetHub

---
+++ 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} boxTypes
+ * @param {number} truckSize
+ * @return {number}
+ */
+var maximumUnits = function(boxTypes, truckSize) {
+    boxTypes.sort((a, b) => b[1] - a[1])
+    console.log("sort: ", boxTypes)
+    let total = 0
+    for(let i=0; i<boxTypes.length; i++) {
+        const num = boxTypes[i][0]
+        const units = boxTypes[i][1]
+        if(truckSize === 0) break
+        if(num <= truckSize) {
+            total += num*units
+            truckSize -= num
+        } else {
+            total += truckSize * units
+            truckSize = 0
+        }
+    }
+    return total
+};
