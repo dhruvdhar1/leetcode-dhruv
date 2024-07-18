d16e8755fb381e289e3c081cc5f6f3a0037514ae
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-18 19:28:48 -0400

    Time: 181 ms (75.22%), Space: 86.4 MB (5.31%) - LeetHub

--- 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.js
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.js
@@ -6,21 +6,18 @@
 var maxIceCream = function(costs, coins) {
     const maxCost = Math.max(...costs)
     const bucket = new Array(maxCost+1).fill(0)
-    let ans = 0
     for(let i=0; i<costs.length; i++) {
-        const cost = costs[i]
-        bucket[cost]++
+        bucket[costs[i]]++
     }
     // console.log("bbb: ", bucket)
-    for(let i=0; i<bucket.length; i++) {
-        if(bucket[i] === 0) continue
-
-        const maxSpend = Number.parseInt(coins/i)
-        const minRequired = Math.min(bucket[i], maxSpend)
-        // console.log(maxSpend, " : ", minRequired)
-        if(coins < i*minRequired) return ans
-        ans += minRequired
-        coins -= i*minRequired
+    let count = 0
+    let i=1
+    while(i < bucket.length) {
+        const buy = Math.min(bucket[i], parseInt(coins/i))
+        // console.log(i, "buy: ", buy)
+        count += buy
+        coins -= (buy*i)
+        i++
     }
-    return ans
+    return count
 };
