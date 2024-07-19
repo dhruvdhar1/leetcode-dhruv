d96a562d49e19295a18241084e74e6e8eabf291b
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-18 21:32:41 -0400

    Time: 48 ms (84.62%), Space: 49.6 MB (30.77%) - LeetHub

--- 1369-minimum-swaps-to-make-strings-equal/1369-minimum-swaps-to-make-strings-equal.js
+++ 1369-minimum-swaps-to-make-strings-equal/1369-minimum-swaps-to-make-strings-equal.js
@@ -4,24 +4,26 @@
  * @return {number}
  */
 var minimumSwap = function(s1, s2) {
-    const map = new Map()
-    const concat = s1+s2
-    for(const ch of concat) {
-        if(!map.has(ch)) {
-            map.set(ch, 0)
-        }
-        const freq = map.get(ch)
-        map.set(ch, freq+1)
-    }
-    for(const [key, val] of map) {
-        if(val%2 !== 0) return -1
-    }
     let xy = 0, yx=0
+    let numx = 0, numy = 0
     for(let i=0; i<s1.length; i++) {
         const ch1 = s1[i]
         const ch2 = s2[i]
-        if(ch1 === "x" && ch2 === "y") xy++
-        else if(ch1 === "y" && ch2 === "x") yx++
+        if(ch1 === 'x') {
+            numx++
+        } else {
+            numy++
+        }
+        if(ch2 === 'x') {
+            numx++
+        } else {
+            numy++
+        }
+        if(ch1 === 'x' && ch2 === 'y') xy++
+        else if(ch1 === 'y' && ch2 === 'x') yx++
     }
+    
+    // console.log(numx, " : ", numy)
+    if(numx%2 !== 0 || numy%2 !== 0) return -1
     return parseInt(xy/2) + xy%2 + parseInt(yx/2) + yx%2
 };
