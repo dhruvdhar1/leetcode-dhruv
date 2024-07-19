24e80229d2c322623eb23ce09b2e6045e824e0dd
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-19 16:02:38 -0400

    Time: 67 ms (74.75%), Space: 51.8 MB (66.67%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.js
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -4,6 +4,7 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
+    const dp = new Map()
     const backtrack = (i, j) => {
         if(i >= s.length && j >= p.length) {
             return true
@@ -11,16 +12,21 @@ var isMatch = function(s, p) {
         if(j >= p.length) {
             return false
         }
-
+        const key = `${i}:${j}`
+        if(dp.has(key)) {
+            return dp.get(key)
+        }
         const match = i < s.length && (s[i] === p[j] || p[j] === ".")
+        let res=null
         if(j+1 < p.length && p[j+1] === '*') {
-           return backtrack(i, j+2) || (match && backtrack(i+1, j))
-        }
-        if(match) {
-            return backtrack(i+1, j+1)
+           res = backtrack(i, j+2) || (match && backtrack(i+1, j))
+        } else if(match) {
+            res = backtrack(i+1, j+1)
         } else {
-            return false
+            res = false
         }
+        dp.set(key, res)
+        return res
     }
 
     return backtrack(0, 0)
