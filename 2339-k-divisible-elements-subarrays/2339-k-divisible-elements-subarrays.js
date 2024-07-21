f2f4c12cb8a60c44f10d2688ce3b5a4679549662
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-20 20:24:11 -0400

    Time: 1024 ms (41.82%), Space: 70.6 MB (50.91%) - LeetHub

--- 2339-k-divisible-elements-subarrays/2339-k-divisible-elements-subarrays.js
+++ 2339-k-divisible-elements-subarrays/2339-k-divisible-elements-subarrays.js
@@ -7,14 +7,13 @@
 var countDistinct = function(nums, k, p) {
     const set = new Set()
     for(let i=0; i<nums.length; i++) {
-        const subarr = []
+        let subarr = []
         let count = 0
         for(let j=i; j<nums.length; j++) {
             if(nums[j]%p === 0) count++
             if(count > k) break
-            const tmp = String([...subarr, nums[j]])
-            set.add(tmp)
             subarr.push(nums[j])
+            set.add(String(subarr))
         }
     }
     return set.size
