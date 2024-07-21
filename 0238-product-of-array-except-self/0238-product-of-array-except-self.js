443fdb9368d7af8d6af75c49ae42ecab05f27314
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-21 14:34:54 -0400

    Time: 97 ms (78.73%), Space: 62.4 MB (80.07%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -14,16 +14,18 @@ var productExceptSelf = function(nums) {
         prod *= nums[i]
     }
     for(let i=0; i<nums.length; i++) {
-        if(nums[i] === 0 && zero === 1) {
-            res[i] = prod
-            continue
-        } 
-        if(zero > 1) {
-            res[i] = 0
-        } else if(zero === 1) {
-            res[i] = 0
+        if(nums[i] !== 0) {
+            if(zero) {
+                res[i] = 0
+            } else {
+                res[i] = prod/nums[i]
+            }
         } else {
-            res[i] = prod/nums[i]
+            if(zero > 1) {
+                res[i] = 0
+            } else {
+                res[i] = prod
+            }
         }
     }
     return res
