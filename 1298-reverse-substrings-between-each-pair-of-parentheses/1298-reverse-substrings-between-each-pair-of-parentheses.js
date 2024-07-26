84f6f9410312b0688fecbb10264becc4fe7c7c8b
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-26 17:04:24 -0400

    Time: 48 ms (90.26%), Space: 50.2 MB (79.66%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.js
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.js
@@ -3,21 +3,28 @@
  * @return {string}
  */
 var reverseParentheses = function(s) {
+    const map = new Map()
     const stack = []
-    const res = []
-    for(const ch of s) {
-        if(ch === ')') {
-            while(stack[stack.length-1] !== '(') {
-                const pop = stack.pop()
-                res.push(pop)
-            }
-            stack.pop() //pop the last opening brace
-            while(res.length > 0) {
-                stack.push(res.shift())
-            }
-        } else {
-            stack.push(ch)
+    for(let i=0; i<s.length; i++) {
+        if(s[i] === '(') {
+            stack.push(i)
+        } else if(s[i] === ')'){
+            const j = stack.pop()
+            map.set(i,j)
+            map.set(j,i)
         }
     }
-    return stack.join("")
+    let res = ""
+    let i=0, increment = 1
+    while(i < s.length) {
+        const ch = s[i]
+        if(ch === '(' || ch === ')') {
+            i = map.get(i)
+            increment = -increment
+        } else {
+            res += s[i]
+        }
+        i += increment
+    }
+    return res
 };
