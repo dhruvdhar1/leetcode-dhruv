b0513b5b0997b6c0ea91c6d4601a4897873429ea
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-30 22:48:40 -0400

    Time: 48 ms (96.63%), Space: 52 MB (9.08%) - LeetHub

---
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+function dfs(root, arr) {
+    if(root === null) return
+    if(root.left === null && root.right === null) {
+        arr.push(root.val)
+        return
+    }
+    dfs(root.left, arr)
+    dfs(root.right, arr)
+}
+var leafSimilar = function(root1, root2) {
+    const arr1 = [], arr2 = []
+    dfs(root1, arr1)
+    dfs(root2, arr2)
+    if(arr1.length !== arr2.length) return false
+    for(let i=0; i<arr1.length; i++) {
+        if(arr1[i] !== arr2[i]) return false
+    }
+    console.log(arr1, " : ", arr2)
+    return true
+};
