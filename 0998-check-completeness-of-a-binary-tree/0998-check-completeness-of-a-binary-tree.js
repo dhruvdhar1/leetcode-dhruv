58e48715ed18ee579cefd1dc3093fb1d15d53634
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-07-31 01:11:53 -0400

    Time: 71 ms (8.12%), Space: 51.1 MB (89.85%) - LeetHub

--- 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.js
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.js
@@ -10,48 +10,20 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-function getHeight(root) {
-    if(null === root) return 0
-    const l = 1 + getHeight(root.left)
-    const r = 1 + getHeight(root.right)
-    return Math.max(l, r)
-}
-function checkLastLevel(arr) {
-    let r = arr.length-1
-    while(arr[r] === null) {
-        r--
-    }
-    while(r >= 0) {
-        if(!arr[r]) return false
-        r--
+
+var isCompleteTree = function(root) {
+    const q = [root]
+    let prevNull = false
+    while(q.length > 0) {
+        const pop = q.shift()
+        if(pop === null) {
+            prevNull = true
+            continue;
+        }
+        if(prevNull) return false
+        q.push(pop.left)
+        q.push(pop.right)
+        
     }
     return true
-}
-var isCompleteTree = function(root) {
-    const totalHeight = getHeight(root)
-    // console.log("th: ", totalHeight)
-    const q = [root]
-    let level = []
-    let height = 0
-    while(q.length > 0 && height < totalHeight) {
-        level = []
-        const size = q.length
-        for(let i=0; i<size; i++) {
-            const pop = q.shift()
-            if(pop === null) {
-                if(height < totalHeight-1) return false
-                level.push(null)
-                continue
-            }
-            level.push(pop.val)
-            const lchild = pop.left
-            const rchild = pop.right
-            q.push(lchild)
-            q.push(rchild)
-        }
-        // console.log("ccc: ", height, " : ", level)
-        height++
-    }
-    // console.log("ccc: ", height, " : ", level)
-    return checkLastLevel(level)
 };
