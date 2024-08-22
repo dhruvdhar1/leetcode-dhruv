914fedc93cc502e850988f3ada676dd7222f8dd6
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-08-22 17:16:43 -0400

    Time: 1 ms (90.01%), Space: 45.9 MB (24.45%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -3,14 +3,11 @@ class Solution {
         int l=0, r=1;
         int totalP = 0;
         while(r < prices.length) {
-            if(prices[r] < prices[l]) {
-                l = r;
-                r = l+1;
-            } else {
+            if(prices[r] > prices[l]) {
                 totalP += prices[r] - prices[l];
-                l = r;
-                r = r+1;
             }
+            l = r;
+            r = r+1;
         }
         return totalP;
     }
