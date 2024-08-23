6b764a1861fbbf5c5f368e1f56893897d0e550ef
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-08-23 11:31:09 -0400

    Time: 0 ms (100%), Space: 40.1 MB (80.26%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,16 @@
 class Solution {
-    private int backtrack(int step, int n, int[] dp) {
-        if(step == n) return 1;
-        if(step > n) return 0;
-        if(dp[step] != 0) return dp[step];
-        
-        int res = backtrack(step+1, n, dp) + backtrack(step+2, n, dp);
-        return dp[step] = res;
+    int backtrack(int n, int[] dp) {
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+        if(dp[n] != -1) return dp[n];
+        dp[n] = backtrack(n-1, dp) + backtrack(n-2, dp);
+        return dp[n];
     }
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
-        return backtrack(0, n, dp);
+        for(int i=0; i<dp.length; i++) {
+            dp[i] = -1;
+        }
+        return backtrack(n, dp);
     }
 }
