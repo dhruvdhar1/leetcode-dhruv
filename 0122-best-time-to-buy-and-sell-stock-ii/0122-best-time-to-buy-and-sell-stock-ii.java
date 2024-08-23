7484bed9cd1c05b377f54d14094bb5bcc342e5ba
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-08-22 22:45:37 -0400

    Time: 3 ms (11.31%), Space: 49.3 MB (5.51%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,24 +1,29 @@
 class Solution {
-    public int backtrack(int[] prices, int ind, boolean canBuy, Map<String, Integer> dp) {
+    public int backtrack(int[] prices, int ind, boolean canBuy, int[][] dp) {
         if(ind >= prices.length) {
             return 0;
         }
-        String key = new StringBuilder().append(ind).append(":").append(canBuy).toString();
-        if(dp.get(key) != null) {
-            return dp.get(key);
+        int yInd = canBuy ? 0 : 1;
+        if(dp[ind][yInd] != -1) {
+            return dp[ind][yInd];
         }
         if(canBuy) {
             int max = Math.max(-prices[ind] + backtrack(prices, ind+1, false, dp), backtrack(prices, ind+1, true, dp));
-            dp.put(key, max);
+            dp[ind][yInd] = max;
             return max;
         } else {
             int max = Math.max(prices[ind] + backtrack(prices, ind+1, true, dp), backtrack(prices, ind+1, false, dp));
-            dp.put(key, max);
+            dp[ind][yInd] = max;
             return max;
         }
     }
     public int maxProfit(int[] prices) {
-        Map<String, Integer> dp = new HashMap();
+        int[][] dp = new int[prices.length+1][2];
+        for(int i=0; i<dp.length; i++) {
+            for(int j=0; j<2; j++) {
+                dp[i][j] = -1;
+            }
+        }
         return backtrack(prices, 0, true, dp);
     }
 }
