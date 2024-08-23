cc3636dc395dae08f5d9ea06ecf92d7064b2b200
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-08-23 15:36:09 -0400

    Time: 143 ms (19.65%), Space: 44.5 MB (30.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,13 +1,3 @@
-/**
-    [10,9,2,5,3,7,101,18]
-    
-    [3,5,6,2,5,4,19,5,6,7,12]
-
-    [2, 4, 5, 6, 7, 12]
-
-        lr
-    [2, 5, 6]
- */
 class Solution {
     int getLowerBound(List<Integer> temp, int num) {
         int l=0, r=temp.size()-1;
@@ -27,13 +17,11 @@ class Solution {
         List<Integer> temp = new ArrayList();
         temp.add(nums[0]);
         for(int i=1; i<nums.length; i++) {
-            // System.out.println("arr: "+ temp.toString());
             int num = nums[i];
             if(num > temp.get(temp.size()-1)) {
                 temp.add(num);
             } else {
                 int ind = getLowerBound(temp, num);
-                // System.out.println("ind: "+ ind);
                 temp.set(ind, num);
             }
         }
