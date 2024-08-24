8f85e482e7d5ca2b0c86de084aaf2d6d7c9c1f8f
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-08-24 18:56:52 -0400

    Time: 19 ms (5.27%), Space: 49.7 MB (5.04%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,18 +1,18 @@
 class Solution {
-    boolean backtrack(String s, int l, int r, boolean canSkip) {
+    boolean backtrack(String s, int l, int r, int k) {
         if(r <= l) return true;
 
         if(s.charAt(l) == s.charAt(r)) {
-            return backtrack(s, l+1, r-1, canSkip);
+            return backtrack(s, l+1, r-1, k);
         } else {
-            if(canSkip) {
-                return backtrack(s, l+1, r, false) || backtrack(s, l, r-1, false);
+            if(k > 0) {
+                return backtrack(s, l+1, r, k-1) || backtrack(s, l, r-1, k-1);
             } else {
                 return false;
             }
         }
     }
     public boolean validPalindrome(String s) {
-        return backtrack(s, 0, s.length()-1, true);
+        return backtrack(s, 0, s.length()-1, 1);
     }
 }
