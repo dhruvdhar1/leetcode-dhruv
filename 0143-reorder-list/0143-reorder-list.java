45f753d6008e41afab7f568844fd48a5074b7e64
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-08-24 12:09:03 -0400

    Time: 2 ms (83.17%), Space: 48.2 MB (35.31%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -36,19 +36,9 @@ class Solution {
         slow.next = null;
 
         p2 = reverseList(p2);
-        // while(p2 != null) {
-        //     System.out.print(p2.val + " : ");
-        //     p2 = p2.next;
-        // }
-        // System.out.print("\n");
-        // while(p1 != null) {
-        //     System.out.print(p1.val + " : ");
-        //     p1 = p1.next;
-        // }
 
         ListNode p1 = head;
         while(p2 != null) {
-            // System.out.println ("p2: "+ p2.val);
             ListNode temp = p2;
             p2 = p2.next;
             temp.next = p1.next;
