364339fa5feb927b85201038e0acdca369f149a8
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-08-23 21:54:42 -0400

    Time: 1 ms (9.27%), Space: 41.8 MB (6.87%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,23 +1,18 @@
 class Solution {
-    int backtrack(int m, int n, int i, int j, Set<String> visited, int[][] dp) {
+    int backtrack(int m, int n, int i, int j, int[][] dp) {
         if(i == m-1 && j == n-1) return 1;
         String key = new StringBuilder().append(i).append(":").append(j).toString();
-        if(i < 0 || i >= m || j < 0 || j >= n || visited.contains(key)) return 0;
+        if(i < 0 || i >= m || j < 0 || j >= n) return 0;
 
         if(dp[i][j] != 0) return dp[i][j];
-        // visited.add(key);
-        int r = backtrack(m, n, i, j+1, visited, dp);
-        int d = backtrack(m, n, i+1, j, visited, dp);
-        // visited.remove(key);
+        int r = backtrack(m, n, i, j+1, dp);
+        int d = backtrack(m, n, i+1, j, dp);
         dp[i][j] = r+d;
         return dp[i][j];
     }
     
     public int uniquePaths(int m, int n) {
         int [][] dp = new int[m+1][n+1];
-        // for(int i=0; i<m; i++) {
-        //     Arrays.fill(dp[i], -1);
-        // }
-        return backtrack(m, n, 0, 0, new HashSet(), dp);
+        return backtrack(m, n, 0, 0, dp);
     }
 }
