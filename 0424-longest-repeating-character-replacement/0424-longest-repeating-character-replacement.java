22e87f74318ddf95ec02a0f0b558cbf52911eef4
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-08-24 12:50:08 -0400

    Time: 47 ms (10.81%), Space: 45.2 MB (6.65%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,31 @@
+class Solution {
+    int getMapFreq(Map<Character, Integer> map) {
+        int max = 0;
+        for(int val: map.values()) {
+            max = Math.max(max, val);
+        }
+        return max;
+    }
+
+    public int characterReplacement(String s, int k) {
+        int l=0, r=0;
+        Map<Character, Integer> map = new HashMap();
+        int longestWindowSize = 0;
+        while(r < s.length()) {
+            char ch = s.charAt(r);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            int maxMapFreq = getMapFreq(map);
+            // System.out.println("ff: "+ maxMapFreq);
+            if((r-l+1 - maxMapFreq) <= k) {
+                longestWindowSize = Math.max(longestWindowSize, r-l+1);
+                r++;
+            } else {
+                char charAtL = s.charAt(l);
+                map.put(charAtL, map.getOrDefault(charAtL, 0) - 1);
+                l++;
+                r++;
+            }
+        }
+        return longestWindowSize;
+    }
+}
