553ce038dc1223c2393997c43ab94acebdc4bbfb
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-08-28 15:32:02 -0400

    Time: 3 ms (53.84%), Space: 43.5 MB (10.51%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -2,16 +2,29 @@ public class Codec {
     ReentrantLock lock;
     Map<String, String> map;
     static final String PREFIX_URL = "http://tinyurl.com/";
-
+    String base62CharSet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public Codec() {
         this.map = new HashMap();
         lock = new ReentrantLock();
     }
 
+    String getId() {
+        char[] id = new char[5];
+        for(int i=0; i<5; i++) {
+            int idx = (int) Math.floor(Math.random() * 62);
+            id[i] = base62CharSet.charAt(idx);
+        }
+        String str = String.valueOf(id);
+        if(map.containsKey(str)) {
+            return getId();
+        }
+        return str;
+    }
+
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
         lock.lock();
-        String id = new StringBuilder("id-").append(map.size()).toString();
+        String id = getId();
         String shortUrl = new StringBuilder(PREFIX_URL).append(id).toString();
         map.put(shortUrl, longUrl);
         lock.unlock();
