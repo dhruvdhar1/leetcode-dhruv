6c9f3266e3c5631f5d4f9435523f49195b48a3e4
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-08-28 10:42:15 -0400

    Time: 753 ms (7.49%), Space: 32.8 MB (36.18%) - LeetHub

--- 2483-task-scheduler-ii/2483-task-scheduler-ii.py
+++ 2483-task-scheduler-ii/2483-task-scheduler-ii.py
@@ -9,12 +9,10 @@ class Solution:
             if task in map:
                 time = max(time+1, map[task] + space + 1)
                 prev_execution_time = map[task]
-                if time - prev_execution_time > space:
-                    map[task] = time
-                    p += 1
+                map[task] = time
             else:
                 time += 1
-                p += 1
                 map[task] = time
+            p += 1
         
         return time
