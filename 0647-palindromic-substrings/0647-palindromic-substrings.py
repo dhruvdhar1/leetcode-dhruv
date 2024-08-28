a94a145d1dbbd2d5c2114e29044dc45112acd4a3
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-08-28 01:14:46 -0400

    Time: 170 ms (45.29%), Space: 16.5 MB (77.8%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getPalindroms(self, s, start, end):
+        print("start: ", start, " , end: ", end)
+        l,r= start,end
+        count=0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            count += 1
+            l -= 1
+            r += 1
+        return count
+
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        for i in range(len(s)):
+            n1 = self.getPalindroms(s, i, i)
+            n2 = self.getPalindroms(s, i, i+1)
+            res += (n1+n2)
+        return res
