746c6f649aa3580295ade60ef7e1204d389890cd
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-08-31 17:07:41 -0400

    Time: 59 ms (20.42%), Space: 48.7 MB (68.33%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.js
+++ 0670-maximum-swap/0670-maximum-swap.js
@@ -9,11 +9,9 @@ var maximumSwap = function(num) {
     for(let i=0; i<nums.length; i++) {
         map.set(+nums[i], i)
     }
-    console.log("map: ", map)
     for(let i=0; i<nums.length; i++) {
         for(j=9; j>nums[i]; j--) {
             if(map.has(j) && map.get(j) > i) {
-                // console.log("here...: ", i, " : ", j, " : ", map.get(j))
                 const temp = nums[i]
                 nums[i] = j
                 nums[map.get(j)] = temp
