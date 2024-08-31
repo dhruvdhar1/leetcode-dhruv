624a409d5b5810acf89e87819898c282b838bac8
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-08-31 16:33:11 -0400

    Time: 62 ms (66.37%), Space: 52.4 MB (64.56%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.js
+++ 0684-redundant-connection/0684-redundant-connection.js
@@ -3,30 +3,31 @@
  * @return {number[]}
  */
 var findRedundantConnection = function(edges) {
-    const parent = []
-    for(let i=0; i<edges.length; i++) {
-        parent[i] = i
-    }
-    const union = (el1, el2) => {
-        const p1 = find(el1)
-        const p2 = find(el2)
+    const parent = edges.map((_, i) => i);
+    const union = (n1, n2) => {
+        const p1 = find(n1)
+        const p2 = find(n2)
         if(p1 !== p2) {
-            parent[p2] = p1
+            parent[parent[p1]] = parent[p2]
         }
     }
-    const find = (el) => {
-        if(el !== parent[el]) parent[el] = find(parent[el])
-        return parent[el]
+    const find = (n1) => {
+        if(parent[n1] !== n1) {
+            return find(parent[parent[n1]])
+        }
+        return n1
     }
-
+    
+    let redundantEdge = null
     for(const edge of edges) {
-        const el1 = edge[0]
-        const el2 = edge[1]
-        const p1 = find(el1)
-        const p2 = find(el2)
+        const p1 = find(edge[0])
+        const p2 = find(edge[1])
 
-        if(p1 === p2) return edge
-        union(el1, el2)
+        if(p1 === p2) {
+            redundantEdge = edge
+        } else {
+            union(edge[0], edge[1])
+        }
     }
-    return null
+    return redundantEdge;
 };
