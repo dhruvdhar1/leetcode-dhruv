5495a69e0e302b03d5aa1149035df3f2b25fa0a1
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-06 14:25:55 -0400

    Time: 95 ms (47.42%), Space: 64.7 MB (57.73%) - LeetHub

--- 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.ts
+++ 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.ts
@@ -1,22 +1,29 @@
 class SparseVector {
-    map = null
+    mat = null
     constructor(nums: number[]) {
-        this.map = new Map()
+        this.mat = []
         for(let i=0; i<nums.length; i++) {
             if(nums[i] !== 0) {
-                this.map.set(i, nums[i])
+                this.mat.push([i, nums[i]])
             }
         }
     }
 
 	// Return the dotProduct of two sparse vectors
     dotProduct(vec: SparseVector): number {
-		const v1 = this.map
-        const v2 = vec.map
+		const v1 = this.mat
+        const v2 = vec.mat
         let res = 0
-        for(const [key, val] of v1) {
-            if(v2.has(key)) {
-                res += val * v2.get(key)
+        let i=0, j=0
+        while(i<v1.length && j<v2.length) {
+            if(v1[i][0] === v2[j][0]) {
+                res += v1[i][1] * v2[j][1]
+                i++
+                j++
+            } else if(v1[i][0] < v2[j][0]) {
+                i++
+            } else {
+                j++
             }
         }
         return res
