49cdc4ec60393da20fc56b6375c39e79259b42ff
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-06 13:29:15 -0400

    Time: 71 ms (5.26%), Space: 51.2 MB (29.26%) - LeetHub

---
+++ 0807-custom-sort-string/0807-custom-sort-string.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} order
+ * @param {string} s
+ * @return {string}
+ */
+var customSortString = function(order, s) {
+    const pq = new MinPriorityQueue({priority: obj => obj[1]})
+    for(let i=0; i<order.length; i++) {
+        const ch = order.charAt(i)
+        pq.enqueue([ch, i])
+    }
+    // console.log("pq: ", pq.toArray())
+    const smap = new Map()
+    for(let i=0; i<s.length; i++) {
+        const ch = s.charAt(i)
+        const f = smap.get(ch) || 0
+        smap.set(ch, f+1)
+    }
+    const res = []
+    while(pq.size() > 0) {
+        const pop = pq.dequeue()
+        const ch = pop.element[0]
+        // console.log("pop: ", ch)
+        // console.log("has: ", smap.get(ch))
+        if(smap.has(ch)) {
+            const f = smap.get(ch)
+            for(let i=0; i<f; i++) {
+                res.push(ch)
+            }
+            smap.delete(ch)
+        }
+    }
+    for(const [key, val] of smap) {
+        for(let i=0; i<val; i++) {
+                res.push(key)
+        }
+    }
+    // console.log("map: ", smap)
+    return res.join('')
+};
