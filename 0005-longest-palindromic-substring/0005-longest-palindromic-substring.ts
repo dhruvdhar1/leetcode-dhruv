893040d7b70383a896eceb3f37fa3973c336b253
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-07 11:30:31 -0400

    Time: 86 ms (70.14%), Space: 54.2 MB (54.89%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.ts
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.ts
@@ -1,28 +1,23 @@
 function longestPalindrome(s: string): string {
 
     const getPalindromeLength = (l, r) => {
-        let res = ""
         while(l >= 0 && r < s.length && s.charAt(l) === s.charAt(r)) {
-            res = s.slice(l, r+1)
             l--
             r++
         }
-        return res
+        return s.slice(l+1,r)
     }
 
     let res = ""
-    for(let i=0; i<s.length; i++) {
+    for(let i=0; i<s.length; i++) { //O(n)
         const str1 = getPalindromeLength(i,i)
         const str2 = getPalindromeLength(i,i+1)
         // console.log("i: ", i, " str1: ", str1, " str2: ", str2)
-        if(str1.length > str2.length) {
-            if(str1.length > res.length) {
-                res = str1
-            }
-        } else {
-            if(str2.length > res.length) {
-                res = str2
-            }
+        if(str1.length > res.length) {
+            res = str1
+        }
+        if(str2.length > res.length){
+            res = str2
         }
     }
     return res
