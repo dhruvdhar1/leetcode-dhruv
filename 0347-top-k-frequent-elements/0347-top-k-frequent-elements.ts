3b199e99256d1e1456a15bf98e2a11502b250379
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-06 21:56:55 -0400

    Time: 81 ms (31.2%), Space: 56.7 MB (34.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.ts
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.ts
@@ -1,18 +1,23 @@
 function topKFrequent(nums: number[], k: number): number[] {
     const map = new Map()
+    const arr = new Array(nums.length+1)
     for(const num of nums) {
         const f = map.get(num) || 0
         map.set(num, f+1)
     }
-    const pq = new MaxPriorityQueue({priority: pair => pair[1]})
-    for(const [key, f] of map) {
-        pq.enqueue([key, f])
+    for(const [key, val] of map) {
+        arr[val] = (arr[val] || new Array())
+        arr[val].push(key)
     }
+    // console.log("arr: ", arr)
     const res = []
-    while(k > 0) {
-        const pop = pq.dequeue()
-        res.push(pop.element[0])
-        k--
+    for(let i = arr.length-1; i>=0; i--) {
+        if(k === 0) break
+        while(arr[i] && arr[i].length > 0 && k > 0) {
+            res.push(arr[i].pop())
+            k--
+        }
     }
+
     return res
 };
