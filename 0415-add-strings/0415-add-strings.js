7b3e56fc542f72839005870e68af004fe9008713
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-08 17:29:05 -0400

    Time: 68 ms (20.28%), Space: 51.9 MB (32.95%) - LeetHub

--- 0415-add-strings/0415-add-strings.js
+++ 0415-add-strings/0415-add-strings.js
@@ -4,25 +4,22 @@
  * @return {string}
  */
 var addStrings = function(num1, num2) {
-    if(num1.length > num2.length) {
-        num2 = num2.padStart(num1.length, '0')
-    } else if(num2.length > num1.length) {
-        num1 = num1.padStart(num2.length, '0')
-    }
+    const maxLen = Math.max(num1.length, num2.length)
+    num2 = num2.padStart(maxLen, '0')
+    num1 = num1.padStart(maxLen, '0')
+    let res = ''
     let carry = 0
-    let arr = []
     for(let i=num1.length-1; i>=0; i--) {
         const int1 = parseInt(num1.charAt(i))
         const int2 = parseInt(num2.charAt(i))
         const sum = int1 + int2 + carry
-        carry = parseInt(sum/10)
         const digit = sum%10
-        arr.push(digit)
+        res = digit + res
+        carry = parseInt(sum/10)
     }
     if(carry) {
-        arr.push(carry)
+        res = carry + res
     }
-    // console.log(arr.reverse().join(''))
-    // arr = arr.reverse()
-    return arr.reverse().join('')
+    // return arr.reverse().join('')
+    return res.toString()
 };
