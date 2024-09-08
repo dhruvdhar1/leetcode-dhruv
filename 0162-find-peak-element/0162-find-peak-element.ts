a11c503cef63e26acaf393ba8e984bcedb28f369
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-07 20:08:52 -0400

    Time: 56 ms (64.81%), Space: 51.1 MB (94.81%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.ts
+++ 0162-find-peak-element/0162-find-peak-element.ts
@@ -1,22 +1,17 @@
 function findPeakElement(nums: number[]): number {
-    let l = 0, r = nums.length-1
+    if(nums.length === 1) {
+        return 0
+    }
+    if(nums[0] > nums[1]) {
+        return 0
+    }
+    if(nums[nums.length-1] > nums[nums.length-2]) {
+        return nums.length-1
+    }
+    let l = 1, r = nums.length-2
     while(l < r) {
         const m = l + Math.floor((r-l)/2)
-        if(m === 0 && m === nums.length-1) {
-            return m
-        } else if(m === 0) {
-            if(nums[m+1] < nums[m]) {
-                return m
-            } else {
-                l = m+1
-            }
-        } else if (m === nums.length-1) {
-            if(nums[m-1] < nums[m]) {
-                return m
-            } else {
-                r = m-1
-            }
-        } else if (nums[m-1] < nums[m] && nums[m+1] < nums[m]){
+        if (nums[m-1] < nums[m] && nums[m+1] < nums[m]){
             return m
         } else if(nums[m-1] > nums[m]) {
             r = m-1
