8d6cbde37e2d1b84992c3129830c8295539e4f23
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-08 20:02:01 -0400

    Time: 71 ms (93.1%), Space: 55.5 MB (89.66%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.ts
@@ -0,0 +1,33 @@
+function exclusiveTime(n: number, logs: string[]): number[] {
+    let prev_time = 0
+    const stack = []
+    const res = new Array(n).fill(0)
+    for(const log of logs) {
+        const tokens = log.split(":")
+        const id = parseInt(tokens[0])
+        const type = tokens[1]
+        const ts = parseInt(tokens[2])
+
+        if(type === "start") {
+            prev_time++
+            if(stack.length === 0) {
+                stack.push(id)
+            } else {
+                const diff = ts - prev_time
+                const last_process = stack[stack.length-1]
+                res[last_process] += diff
+                stack.push(id)
+            }
+            prev_time = ts
+        } else {
+            stack.pop()
+            const runtime = ts - prev_time + 1
+            res[id] += runtime
+            prev_time = ts
+        }
+
+    }
+    console.log(stack)
+    return res
+
+};
