8e116715d9404d55646a144f7c32c8aa1c3b1cf3
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-10 16:11:59 -0400

    Time: 245 ms (20.07%), Space: 66 MB (18.46%) - LeetHub

---
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var shortestPathBinaryMatrix = function(grid) {
+    const q = [[0,0,1]]
+    const visited = new Set()
+    visited.add([0,0])
+
+    while(q.length > 0) {
+        const pop = q.shift()
+        const x = pop[0]
+        const y = pop[1]
+        const len = pop[2]
+        const key = `${x}:${y}`
+        if(x < 0 || y < 0 || x >= grid.length || y >= grid.length || grid[x][y] === 1 || visited.has(key)) {
+            continue
+        }
+        if(x === grid.length-1 && y === grid.length-1 && grid[x][y] === 0) {
+            return len
+        }
+        q.push([x+1, y, len+1])
+        q.push([x, y+1, len+1])
+        q.push([x+1, y+1, len+1])
+        q.push([x+1, y-1, len+1])
+        q.push([x, y-1, len+1])
+        q.push([x-1, y, len+1])
+        q.push([x-1, y-1, len+1])
+        q.push([x-1, y+1, len+1])
+        visited.add(key)
+    }
+    return -1
+};
