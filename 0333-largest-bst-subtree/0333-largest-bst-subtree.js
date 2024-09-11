80e216b9286171c6a4a2a586871873192193b4a9
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-11 14:00:10 -0400

    Time: 67 ms (50.56%), Space: 54.8 MB (48.31%) - LeetHub

--- 0333-largest-bst-subtree/0333-largest-bst-subtree.js
+++ 0333-largest-bst-subtree/0333-largest-bst-subtree.js
@@ -10,24 +10,22 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var largestBSTSubtree = function(root) {
-    function dfs(node) {
-        if(node === null) {
-            return [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER, 0]
-        }
-
-        const l = dfs(node.left)
-        const r = dfs(node.right)
-
-        if(node.val > l[1] && node.val < r[0]) {
-            const min = Math.min(node.val, l[0])
-            const max = Math.max(node.val, r[1])
-            const size = l[2] + r[2] + 1
-            return [min, max, size]
-        }
-        return [Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, Math.max(l[2], r[2])]
+function dfs(root) {
+    if(root == null) {
+        return [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER, 0]
     }
-    const ans = dfs(root)
-    console.log(ans)
-    return ans[2]
+    const l = dfs(root.left)
+    const r = dfs(root.right)
+    // console.log("l: ", l, " r: ", r)
+    if(root.val > l[1] && root.val < r[0]) {
+        const min = Math.min(root.val, l[0])
+        const max = Math.max(root.val, r[1])
+        const count = 1 + l[2] + r[2]
+        return [min, max,count]
+    }
+    return [Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, Math.max(l[2], r[2])]
+}
+var largestBSTSubtree = function(root) {
+    const res = dfs(root)
+    return res[2]
 };
