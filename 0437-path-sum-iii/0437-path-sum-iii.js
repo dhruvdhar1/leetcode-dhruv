d7e68cbe47dca4718d79903981a701da6f6f94bb
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-11 10:16:42 -0400

    Time: 160 ms (6.7%), Space: 60.2 MB (15.88%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.js
+++ 0437-path-sum-iii/0437-path-sum-iii.js
@@ -11,33 +11,34 @@
  * @param {number} targetSum
  * @return {number}
  */
-let count = 0
-function dfs(node, sum, target, map, paths) {
-    if(node === null) {
-        return
-    }
-    const currSum = sum + node.val
-    const diff = currSum - target
-    if(map.has(diff)) {
-        paths[0] += map.get(diff)
-    }
-    if(map.has(currSum)) {
-        map.set(currSum, map.get(currSum) + 1)
-    } else {
-        map.set(currSum, 1)
-    }
-    dfs(node.left, sum+node.val, target, map, paths)
-    dfs(node.right, sum+node.val, target, map, paths)
 
-    map.set(currSum, map.get(currSum) - 1)
-    if(map.get(currSum) === 0) {
-        map.delete(currSum)
-    }
-}
 var pathSum = function(root, targetSum) {
-    let paths = [0]
-    const map = new Map()
-    map.set(0, 1)
-    dfs(root, 0, targetSum, map, paths)
-    return paths[0]
+    const map = new Map([[0,1]])
+    let count = 0
+    function dfs(node, sum) {
+        if(node == null) return 
+
+        const currSum = sum + node.val
+        const diff = currSum - targetSum
+        console.log("diff: ", diff, " : ", currSum)
+        if(map.has(diff)) {
+            console.log("cnt: ", map.get(diff))
+            count += map.get(diff) || 0
+        }
+        if(map.has(currSum)) {
+            map.set(currSum, map.get(currSum) + 1)
+        } else {
+            map.set(currSum, 1)
+        }
+        dfs(node.left, currSum)
+        dfs(node.right, currSum)
+
+        map.set(currSum, map.get(currSum)-1)
+        if(map.get(currSum) === 0) {
+            map.delete(currSum)
+        }
+    }
+
+    dfs(root, 0)
+    return count
 };
