0d122b23a4323eb09ae22a9b4c2a5890da9a964e
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-10 20:55:01 -0400

    Time: 658 ms (10.93%), Space: 100.2 MB (79.83%) - LeetHub

---
+++ 2019-product-of-two-run-length-encoded-arrays/2019-product-of-two-run-length-encoded-arrays.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} encoded1
+ * @param {number[][]} encoded2
+ * @return {number[][]}
+ */
+var findRLEArray = function(encoded1, encoded2) {
+    let p=0, q=0
+    let res = []
+    while (p < encoded1.length && q < encoded2.length) {
+        const prod = encoded1[p][0] * encoded2[q][0]
+        // console.log(p, " : ", q, " : ", encoded1[p][0], " : ", encoded2[q][0])
+        const freq = Math.min(encoded1[p][1], encoded2[q][1])
+        if(res.length > 0 && prod === res[res.length-1][0]) {
+            res[res.length-1][1] += freq
+        } else {
+            res.push([prod, freq])
+        }
+        encoded1[p][1] -= freq
+        encoded2[q][1] -= freq
+        if(encoded1[p][1] < encoded2[q][1]) {
+            p++
+        } else if (encoded1[p][1] > encoded2[q][1]) {
+            q++
+        } else {
+            p++
+            q++
+        }
+    }
+     
+    return res
+};
