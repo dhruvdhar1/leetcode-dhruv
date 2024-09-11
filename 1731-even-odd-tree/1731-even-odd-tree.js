0be1312800d60cd8f5da2b2fc2e9570dbb2b4a44
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-10 20:09:19 -0400

    Time: 228 ms (21.31%), Space: 81.5 MB (61.48%) - LeetHub

--- 1731-even-odd-tree/1731-even-odd-tree.js
+++ 1731-even-odd-tree/1731-even-odd-tree.js
@@ -10,41 +10,12 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-// function checkIncreasing(q) {
-//     if(q[0] && q[0].val % 2 === 0) return false
-//     for(let i=1; i<q.length; i++) {
-//         const curr = q[i]
-//         const prev = q[i-1]
-//         if(curr.val % 2 === 0 || prev.val % 2 === 0 || prev.val >= curr.val) {
-//             return false
-//         }
-//     }
-//     return true
-// }
-
-// function checkDecreasing(q) {
-//     if(q[0] && q[0].val % 2 !== 0) return false
-//     for(let i=1; i<q.length; i++) {
-//         const curr = q[i]
-//         const prev = q[i-1]
-//         if(curr.val % 2 !== 0 || prev.val % 2 !== 0 || prev.val <= curr.val) {
-//             return false
-//         }
-//     }
-//     return true
-// }
+ 
 var isEvenOddTree = function(root) {
     const q = [root] //dequeue
     let level = 0
 
     while(q.length > 0) {
-        // if(level%2 === 0) {
-        //     const increasing = checkIncreasing(q)
-        //     if(!increasing) return false
-        // } else {
-        //     const decreasing = checkDecreasing(q)
-        //     if(!decreasing) return false
-        // }
         const aux = []
         let prev = null
         while(q.length > 0) {
