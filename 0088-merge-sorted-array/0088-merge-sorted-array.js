42618d5b1fc523d1855a6daabfa07ef87a457283
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-12 01:11:42 -0400

    Time: 63 ms (11.02%), Space: 52.3 MB (5.15%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,20 +6,30 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let q = n-1
-    let p = m-1
-    let i = nums1.length - 1
-
-    while(p >=0 && q >= 0) {
-        if(nums1[p] >= nums2[q]) {
-            nums1[i--] = nums1[p--]
+    let l = 0, r = nums1.length-nums2.length-1
+    while(l < r) {
+        const temp = nums1[r]
+        nums1[r] = nums1[l]
+        nums1[l] = temp
+        l++
+        r--
+    }
+    r = nums1.length-nums2.length-1
+    console.log("n1: ", nums1, " : ", r)
+    l = 0
+    let p = nums1.length-1
+    while(l<nums2.length && r >= 0) {
+        if(nums1[r] <= nums2[l]) {
+            nums1[p--] = nums1[r]
+            r--
         } else {
-            nums1[i--] = nums2[q--]
+            nums1[p--] = nums2[l]
+            l++
         }
     }
-
-    while(q >= 0) {
-        nums1[i--] = nums2[q--]
+    // p = 0
+    while(l < nums2.length) {
+        nums1[p--] = nums2[l++]
     }
-
+    nums1.reverse()
 };
