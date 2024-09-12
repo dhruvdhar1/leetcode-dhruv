2c80679513779ad8f3ad075de6c7a92d0e009569
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-12 02:07:42 -0400

    Time: 136 ms (39.18%), Space: 69.9 MB (6.19%) - LeetHub

--- 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.js
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.js
@@ -13,21 +13,28 @@
  */
 
 var lowestCommonAncestor = function(root, p, q) {
-    let sum = 0
-    const lca = (node, p, q) => {
-        if(node === null) return null;
+    let pFound = false, qFound = false
+    function dfs(node) {
+        if(node == null) return null
 
-        const l = lca(node.left, p, q);
-        const r = lca(node.right, p, q);
-        if(node === p || node === q) {
-            sum++;
-            return node;
+        const l = dfs(node.left)
+        const r = dfs(node.right)
+
+        if(l === p || r === p || node === p) {
+            pFound = true
+        }
+        if(l === q || r === q || node === q) {
+            qFound = true
         }
 
-        if(l && r) return node;
-        return l || r;
-    }
-    const res = lca(root, p, q)
-    if(sum !== 2) return null
-    return res
+        if((l === p || l === q) && (r === q || r === p)) {
+            return node
+        } else if(node === p || node === q) {
+            return node
+        }
+        return l || r
+    }   
+
+    const lca = dfs(root)
+    return pFound && qFound ? lca : null
 };
