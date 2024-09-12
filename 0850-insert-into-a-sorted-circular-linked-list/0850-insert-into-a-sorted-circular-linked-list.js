9f54481ba5ea9495f17ed1f91e71b107bd88b9aa
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-12 11:13:48 -0400

    Time: 59 ms (68.18%), Space: 51.6 MB (83.33%) - LeetHub

--- 0850-insert-into-a-sorted-circular-linked-list/0850-insert-into-a-sorted-circular-linked-list.js
+++ 0850-insert-into-a-sorted-circular-linked-list/0850-insert-into-a-sorted-circular-linked-list.js
@@ -11,32 +11,34 @@
  * @param {number} insertVal
  * @return {_Node}
  */
-
-const insertHelper = (prev, next, val) => {
+function insertNode(prev, next, val) {
     const node = new _Node(val)
     prev.next = node
     node.next = next
 }
 var insert = function(head, insertVal) {
-    if(head == null) {
+    if(head === null) {
         const node = new _Node(insertVal)
         node.next = node
         return node
     }
-
     let curr = head
-    while(curr.next != head) {
-        if(insertVal >= curr.val && insertVal <= curr.next.val) {
-            insertHelper(curr, curr.next, insertVal)
+    let next = curr.next
+
+    while(curr.next !== head) {
+        if(insertVal >= curr.val && insertVal <= next.val) {
+            insertNode(curr, next, insertVal)
             return head
-        } else if(curr.val > curr.next.val) {
-            if(insertVal >= curr.val || insertVal <= curr.next.val) {
-                insertHelper(curr, curr.next, insertVal)
+        } 
+        if(curr.val > next.val) {
+            if(insertVal >= curr.val || insertVal <= next.val) {
+                insertNode(curr, next, insertVal)
                 return head
             }
         }
         curr = curr.next
+        next = next.next
     }
-    insertHelper(curr, curr.next, insertVal)
+    insertNode(curr, next, insertVal)
     return head
 };
