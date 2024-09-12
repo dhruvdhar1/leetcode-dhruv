c590f787a684820440e9069008a5a537c4297572
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-12 10:31:21 -0400

    Time: 85 ms (19.71%), Space: 56.4 MB (29.64%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -3,41 +3,38 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
+
+function isCycle(adj, node, visited) {
+    if(visited.has(node)) return true
+
+    visited.add(node)
+    const neigh = adj.get(node) || []
+    for(const n of neigh) {
+        const cycle = isCycle(adj, n, visited)
+        if(cycle) return true
+    }
+    visited.delete(node)
+    adj.set(node, [])
+    return false
+}
 var canFinish = function(numCourses, prerequisites) {
-    if(prerequisites.length === 0) return true
-    function checkCycle(node, visited, map) {
-        if(visited.has(node)) return true
-        visited.add(node)
-        const neighbors = map.get(node)
-        let cycle = false
-        for(const n of neighbors) {
-            cycle = cycle || checkCycle(n, visited, map)
-            if(cycle) return true
+    const adj = new Map()
+    for(const prereq of prerequisites) {
+        const course1 = prereq[0]
+        const course2 = prereq[1]
+        // course2 --> course1
+        if(!adj.has(course2)) {
+            adj.set(course2, [])
         }
-        visited.delete(node)
-        map.set(node, [])
-        return cycle
+        adj.get(course2).push(course1)
     }
 
-    const map = new Map()
     const visited = new Set()
-    for(const prereq of prerequisites) {
-        const to = prereq[1]
-        const from = prereq[0]
-        if(!map.has(from)) {
-            map.set(from, [])
+    for(const [key, _] of adj) {
+        if(!visited.has(key)) {
+            const cycle = isCycle(adj, key, visited)
+            if(cycle) return false
         }
-        if(!map.has(to)) {
-            map.set(to, [])
-        }
-        const arr = map.get(from)
-        arr.push(to)
-        map.set(from, arr)
-    }
-    // console.log(map)
-    for(const [key, val] of map) {
-        const cycle = checkCycle(key, visited, map)
-        if(cycle) return false
     }
     return true
 };
