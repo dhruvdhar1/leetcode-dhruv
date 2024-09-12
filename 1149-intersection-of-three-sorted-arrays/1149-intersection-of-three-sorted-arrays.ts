ccdf4b4632eebc2f22f4de85ca4c0c09a14b1a2a
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-12 11:59:59 -0400

    Time: 59 ms (73.08%), Space: 52.8 MB (19.23%) - LeetHub

--- 1149-intersection-of-three-sorted-arrays/1149-intersection-of-three-sorted-arrays.ts
+++ 1149-intersection-of-three-sorted-arrays/1149-intersection-of-three-sorted-arrays.ts
@@ -1,16 +1,23 @@
 function arraysIntersection(arr1: number[], arr2: number[], arr3: number[]): number[] {
-    let set = new Set(arr1)
-    let res = []
-    for(const num of arr2) {
-        if(set.has(num)) {
-            res.push(num)
-        }
-    }
-    set = new Set(res)
-    res = []
-    for(const num of arr3) {
-        if(set.has(num)) {
-            res.push(num)
+    let p = 0, q = 0, r = 0
+    const res = []
+    while(p < arr1.length && q < arr2.length && r < arr3.length) {
+        if(arr1[p] === arr2[q] && arr2[q] === arr3[r]) {
+            res.push(arr1[p])
+            p++
+            q++
+            r++
+        } else {
+            const max = Math.max(arr1[p], arr2[q], arr3[r])
+            if(arr1[p] < max) {
+                p++
+            }
+            if(arr2[q] < max) {
+                q++
+            }
+            if(arr3[r] < max) {
+                r++
+            }
         }
     }
     return res
