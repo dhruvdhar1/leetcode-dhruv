fa685e9553f0d0feb68759b07eeea2f4ee9c5fac
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-12 15:13:35 -0400

    Time: 73 ms (26.29%), Space: 54.1 MB (19.02%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.js
+++ 0043-multiply-strings/0043-multiply-strings.js
@@ -4,69 +4,25 @@
  * @return {string}
  */
 var multiply = function(num1, num2) {
-    let maxlen = Math.max(num1.length, num2.length)
-    num1 = num1.padStart(maxlen, '0')
-    num2 = num2.padStart(maxlen, '0')
-
-    const res = []
-    let carry = 0
-    maxlen=0
-    for(let i = num2.length-1; i>=0; i--) {
-        // console.log("i: ", i)
-        const arr = []
-        for(k=0; k<num2.length-i-1; k++) {
-            arr.push(0)
+    if(num1 === '0' || num2 === '0') return '0'
+    nums1 = num1.split('').reverse()
+    nums2 = num2.split('').reverse()
+    const res = new Array(num1.length + num2.length).fill(0)
+    for(let i=0; i<nums1.length; i++) {
+        for(let j=0; j<nums2.length; j++) {
+            const digit = nums1[i] * nums2[j]
+            res[i+j] += digit
+            res[i+j+1] += parseInt(res[i+j]/10)
+            res[i+j] = res[i+j]%10
         }
-        for(let j=num1.length-1; j>=0; j--) {
-            // console.log("j: ", j)
-            const n1 = parseInt(num1.charAt(j))
-            const n2 = parseInt(num2.charAt(i))
-            const prod = n1 * n2 + carry
-            carry = parseInt(prod/10)
-            const n = prod%10
-            arr.push(n)
-        }
-        if(carry) {
-            arr.push(carry)
-        }
-        carry = 0
-        arr.reverse()
-        const str = arr.join('')
-        maxlen = Math.max(maxlen, str.length)
-        res.push(str)
     }
-    for(let i=0; i<res.length; i++) {
-        res[i] = res[i].padStart(maxlen, '0')
-    }
-    console.log(res)
-    const sum = calcSum(res)
-    const str = String(sum)
-    let leadingzInd = 0
-    for(let i=0; i<str.length-1; i++) {
-        if(str.charAt(i) === '0') {
-            leadingzInd++
-        } else {
+    res.reverse()
+    const str = res.join('')
+    let i=0
+    for(i=0; i<str.length; i++) {
+        if(str.charAt(i) !== '0') {
             break
         }
     }
-    return str.slice(leadingzInd, str.length)
+    return str.slice(i, str.length)
 };
-
-function calcSum(sums) {
-    const res = []
-    let carry = 0
-    for(let i=sums[0].length-1; i>=0; i--) {
-        let sum = carry
-        for(let j=sums.length-1; j>=0; j--) {
-            sum += parseInt(sums[j][i])
-        }
-        const n = sum%10
-        carry = parseInt(sum/10)
-        res.push(n)
-    }
-    if(carry) {
-        res.push(carry)
-    }
-    res.reverse()
-    return res.join('')
-}
