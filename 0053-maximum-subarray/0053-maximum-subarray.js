840b34dee133dfaeae5bd40c61858a1342e12945
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-12 19:43:55 -0400

    Time: 62 ms (92.24%), Space: 57.8 MB (47.22%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,12 +3,11 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let maxSum = nums[0]
-    let currSum = nums[0]
+    let sum = nums[0]
+    let maxSum = sum
     for(let i=1; i<nums.length; i++) {
-        const num = nums[i]
-        currSum = Math.max(currSum+num, num)
-        maxSum = Math.max(maxSum, currSum)
+        sum = Math.max(nums[i], sum + nums[i])
+        maxSum = Math.max(maxSum, sum)
     }
     return maxSum
 };
