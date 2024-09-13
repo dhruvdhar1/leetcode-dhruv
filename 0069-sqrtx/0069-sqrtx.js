665b2d4922e532e1f326bae273eafd5fbcb4c991
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-13 01:30:45 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -6,7 +6,7 @@ var mySqrt = function(x) {
     if(x === 0 || x === 1) return x
     let l = 1, r = x
 
-    while(l < r) {
+    while(l <= r) {
         const mid = l + parseInt((r-l)/2)
         console.log(mid)
         if(mid * mid === x) return mid
@@ -15,5 +15,5 @@ var mySqrt = function(x) {
             r = mid-1
         }
     }
-    return r-1
+    return l-1
 };
