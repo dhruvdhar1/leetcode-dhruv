58f26b134ffd64fb2090d3cdeb7d4a8c72698116
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-12 21:51:29 -0400

    Time: 1570 ms (18.51%), Space: 54.6 MB (43.76%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -3,28 +3,33 @@
  * @param {string} word
  * @return {boolean}
  */
-var exist = function(board, word) {
-    function dfs(i, j, m, n, ind) {
-        const key = `${i}:${j}`
-        if(ind >= word.length) return true
-        if(i >= m || i < 0 || j >= n || j < 0) return false
-        if(board[i][j] !== word[ind]) return false
-        const temp = board[i][j]
-        board[i][j] = '\0'
-        const a = dfs(i+1, j, m, n, ind+1)
-        const b = dfs(i-1, j, m, n, ind+1)
-        const c = dfs(i, j+1, m, n, ind+1)
-        const d = dfs(i, j-1, m, n, ind+1)
-        board[i][j] = temp
-        return a || b || c || d
+
+function dfs(i, j, m, n, visited, word, ind, board) {
+    const key = `${i}:${j}`
+    // console.log("key: ", key)
+    // console.log("i: ", ind, " : ", key)
+    if(i < 0 || j < 0 || i >= m || j >= n 
+            || visited.has(key) || word.charAt(ind) !== board[i][j]) {
+        return false
     }
-    let m = board.length
-    let n = board[0].length
-    let res = false
-    for(let i=0; i<m; i++) {
-        for(let j=0; j<n; j++) {
-            if(board[i][j] === word[0]) {
-                res = dfs(i, j, m, n, 0)
+    if(ind == word.length-1 && word.charAt(ind) === board[i][j]) {
+        return true
+    }
+
+    visited.add(key)
+    const a = dfs(i+1, j, m, n, visited, word, ind+1, board)
+    const b = dfs(i-1, j, m, n, visited, word, ind+1, board)
+    const c = dfs(i, j+1, m, n, visited, word, ind+1, board)
+    const d = dfs(i, j-1, m, n, visited, word, ind+1, board)
+    visited.delete(key)
+    return a || b || c || d
+}
+var exist = function(board, word) {
+    const visited = new Set()
+    for(let i=0; i<board.length; i++) {
+        for(let j=0; j<board[0].length; j++) {
+            if(board[i][j] === word.charAt(0)) {
+                const res = dfs(i, j, board.length, board[0].length, visited, word, 0, board)
                 if(res) return true
             }
         }
