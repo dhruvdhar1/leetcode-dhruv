59c36b9cf438b12007deb914bfc664ae0941a366
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-13 15:13:12 -0400

    Time: 178 ms (5.09%), Space: 60.9 MB (5.09%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var minMeetingRooms = function(intervals) {
+    const pq = new MinPriorityQueue({priority: interval => interval[1]})
+    intervals.sort((a, b) => a[0] - b[0])
+    let max = -1
+    for(const interval of intervals) {
+        const front = pq.front() //.element
+        if(front == null || interval[0] < front.element[1]) {
+            pq.enqueue(interval)
+            console.log("fff: ", pq.toArray())
+            max = Math.max(max, pq.size())
+        } else {
+            pq.dequeue()
+            pq.enqueue(interval)
+        }
+    }
+    return max
+};
