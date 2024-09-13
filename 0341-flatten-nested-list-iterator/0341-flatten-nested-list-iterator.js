144d3d862afe83c6b574ea35b6be6d4b59d87196
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-13 15:51:44 -0400

    Time: 92 ms (13.09%), Space: 63.3 MB (5.36%) - LeetHub

---
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.js
@@ -0,0 +1,65 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * function NestedInteger() {
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     @return {boolean}
+ *     this.isInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     @return {integer}
+ *     this.getInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     Return null if this NestedInteger holds a single integer
+ *     @return {NestedInteger[]}
+ *     this.getList = function() {
+ *         ...
+ *     };
+ * };
+ */
+/**
+ * @constructor
+ * @param {NestedInteger[]} nestedList
+ */
+
+class NestedIterator {
+    flat = null
+    constructor(nestedList) {
+        const list = []
+		this.flatten(nestedList, list)
+        this.flat = new Queue(list)
+    }
+
+    flatten(nestedList, list) {
+        for(const el of nestedList) {
+            if(el.isInteger()) {
+                list.push(el)
+            } else {
+                this.flatten(el.getList(), list)
+            }
+        }
+    }
+
+    hasNext() {
+		return this.flat.size() > 0
+    }
+
+	next() {
+		const pop = this.flat.dequeue()
+        return pop.getInteger()
+    }
+}
+
+
+/**
+ * Your NestedIterator will be called like this:
+ * var i = new NestedIterator(nestedList), a = [];
+ * while (i.hasNext()) a.push(i.next());
+*/
