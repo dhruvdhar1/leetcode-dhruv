f0d17ff8de5da60ca9e4eff5422e073685a31d51
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-13 15:15:40 -0400

    Time: 101 ms (6.68%), Space: 54.5 MB (25.91%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
@@ -5,12 +5,10 @@
 var minMeetingRooms = function(intervals) {
     const pq = new MinPriorityQueue({priority: interval => interval[1]})
     intervals.sort((a, b) => a[0] - b[0])
-    let max = -1
     for(const interval of intervals) {
         const front = pq.front()
         if(front == null || interval[0] < front.element[1]) {
             pq.enqueue(interval)
-            max = Math.max(max, pq.size())
         } else {
             pq.dequeue()
             pq.enqueue(interval)
