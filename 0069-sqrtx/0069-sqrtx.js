c396cd75f989ed3b5a9c87f614c9699a9240cfdf
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-13 09:02:17 -0400

    Time: 61 ms (95.36%), Space: 53.4 MB (9.56%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    if(x === 0 || x === 1) return x
-    let l = 1, r = x
-
+    let l=1, r = x
     while(l <= r) {
-        const mid = l + Math.floor((r-l)/2)
-
-        if(mid * mid === x) return mid
-        if(mid * mid <= x) l = mid+1
-        else {
+        const mid = l + parseInt((r-l)/2)
+        const sq = mid*mid
+        if(sq === x) return mid
+        else if(sq > x) {
             r = mid-1
+        } else {
+            l = mid+1
         }
     }
     return l-1
