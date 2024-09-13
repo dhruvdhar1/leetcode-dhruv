454a127402352d9cfb6c61363b04781c10bc39e5
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-13 12:36:40 -0400

    Time: 80 ms (62.19%), Space: 59.3 MB (10.97%) - LeetHub

--- 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.js
+++ 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.js
@@ -3,24 +3,23 @@
  * @return {number}
  */
 function isValid(age1, age2) {
+    if(age2 <= 0.5*age1+7) return false
     if(age2 > age1) return false
-    if(age2 <= (0.5*age1+7)) return false
-    // if(age2 > 100 && age1 < 100) return false
     return true
 }
 var numFriendRequests = function(ages) {
     const map = new Map()
     for(const age of ages) {
-        map.set(age, (map.get(age) || 0) + 1)
+        const f = map.get(age) || 0
+        map.set(age, f+1)
     }
-    console.log(map)
     let res = 0
-    for(const [age1, count1] of map) {
-        for(const [age2, count2] of map) {
+    for(const [age1, f1] of map) {
+        for(const [age2, f2] of map) {
             if(isValid(age1, age2)) {
-                res += count1 * count2
+                res += f1*f2
                 if(age1 === age2) {
-                    res -= count2
+                    res -= f1
                 }
             }
         }
