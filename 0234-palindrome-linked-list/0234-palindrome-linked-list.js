abf9f1d4472499f13da50d9064df1c0c6fb23f12
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-13 14:58:06 -0400

    Time: 130 ms (62.37%), Space: 76.8 MB (32.38%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -16,18 +16,7 @@
     2 --> 1    3  null
     ^-----------
  */
-function reverseList( head) {
-    let curr = head, prev = null, next
-    while (curr != null) {
-        next = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = next;
-    }
-        
-    // Return the head of reversed linked list
-    return prev;
-    }
+
 function reverse(head) {
     if(head === null) return null
     let curr = head, next = curr.next
@@ -47,15 +36,10 @@ var isPalindrome = function(head) {
         slow = slow.next
         fast = fast.next.next
     }
-    // if(slow === fast) {
-    //     return slow.val === slow.next.val
-    // }
-    const rev = reverseList(slow.next)
-    console.log("rev: ", rev)
+    const rev = reverse(slow.next)
     slow.next = rev
 
     let p1 = head, p2 = slow.next
-    console.log("p: ", p1)
     while(p1 != null && p2 != null) {
         if(p1.val != p2.val) return false
         p1 = p1.next
