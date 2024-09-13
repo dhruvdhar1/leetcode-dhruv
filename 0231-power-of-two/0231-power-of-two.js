78babb5ea7632d6049645430ddf25208e4f8da91
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-13 00:22:43 -0400

    Time: 76 ms (54.34%), Space: 53.6 MB (18.86%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -6,6 +6,5 @@ var isPowerOfTwo = function(n) {
     if(n === 0) return false
     if(n === 1) return true
     const power = Math.log(n)/Math.log(2)
-    console.log(Math.floor(power), " : ", Math.ceil(power))
     return Math.floor(power.toFixed(10)) === Math.ceil(power.toFixed(10))
 };
