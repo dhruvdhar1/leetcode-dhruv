78df7517a106dc303d74f6a01a50d1c7f8ca04b9
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-13 12:26:54 -0400

    Time: 54 ms (37.97%), Space: 48.9 MB (34.93%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -4,26 +4,26 @@
  */
 var decodeString = function(s) {
     const stack = []
-    for(let i=0; i<s.length; i++) {
-        const ch = s.charAt(i)
+    for(const ch of s) {
         if(ch === ']') {
-            let num = ''
-            let str = ''
-            while(stack[stack.length-1] != '[') {
+            let str = ""
+            let num = ""
+            while(stack[stack.length-1] !== '[') {
                 str = stack.pop() + str
             }
             stack.pop()
             while(stack[stack.length-1] >= '0' && stack[stack.length-1] <= '9') {
+                // console.log(stack[stack.length-1])
                 num = stack.pop() + num
             }
+            console.log(str, ' : ', num)
             const seq = str.repeat(parseInt(num))
-            console.log(seq)
             stack.push(seq)
         } else {
             stack.push(ch)
         }
     }
-    let res = ''
+    let res = ""
     while(stack.length > 0) {
         res = stack.pop() + res
     }
