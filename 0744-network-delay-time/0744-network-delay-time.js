08b85ee678b63483c5857d6149b45d7269860248
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-14 11:30:19 -0400

    Time: 304 ms (10.86%), Space: 69.9 MB (5.02%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.js
+++ 0744-network-delay-time/0744-network-delay-time.js
@@ -1,44 +1,51 @@
 /**
- * @param {number[][]} times
- * @param {number} n
- * @param {number} k
- * @return {number}
+    {
+        2: [[1,1], [3,1]]
+        3: [[4,1]][5, 1]
+    }
+    n=4, k=2
+    maxHeap: [[1,1][3,1]]
+    {
+        1:1
+        3:1
+        4:1
+    }
+
  */
 var networkDelayTime = function(times, n, k) {
     const adj = new Map()
+    const dists = new Map([[k, 0]])
     const visited = new Set()
-    const distance = new Map()
-    const pq = new MinPriorityQueue({priority: el => el[1]})
-    pq.enqueue([k, 0])
     for(const time of times) {
         const src = time[0]
         const dest = time[1]
         const w = time[2]
-
         if(!adj.has(src)) {
             adj.set(src, [])
         }
         adj.get(src).push([dest, w])
     }
-
+    console.log(adj)
+    const pq = new MinPriorityQueue({priority: el => el[1]})
+    pq.enqueue([k,0])
     while(pq.size() > 0) {
         const pop = pq.dequeue().element
         const node = pop[0]
-        const time = pop[1] //=== -1 ? 0 : pop[1]
-        if(!distance.has(node) || distance.get(node) > time) {
-            distance.set(node, time)
+        const w = pop[1]
+
+        if(!dists.has(node) || dists.get(node) > w) {
+            dists.set(node, w)
         }
+
         const neigh = adj.get(node) || []
         for(const n of neigh) {
             if(!visited.has(n[0])) {
-                const dest = n[0]
-                const weight = n[1]
-                pq.enqueue([dest, time+weight])
+                const newDist = w + n[1]
+                pq.enqueue([n[0], newDist])
             }
         }
         visited.add(node)
     }
-    // console.log("dist: ", distance.values())
-    if(distance.size != n) return -1
-    return Math.max(...distance.values())
+    if(dists.size != n) return -1
+    return Math.max(...dists.values())
 };
