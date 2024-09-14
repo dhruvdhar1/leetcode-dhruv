654dc4339f8f0a3e2b203ec73b3329431de087ad
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-14 17:26:40 -0400

    Time: 64 ms (96.35%), Space: 52.9 MB (87.12%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -1,38 +1,49 @@
 /**
-   0    1   2   3
-0  0    0   0   0
-1  0    4   5   0
-2  0    3   1   0
+        0    1   2   3
 
+0       0    0   0   0
+1       0    4   5   0
+2       0    3   1   0
+
+fr = 0
+fc = 0
 [r0, c0, c3]
  */
 var setZeroes = function(matrix) {
-    const set = new Set()
-    const m = matrix.length, n = matrix[0].length
+    let firstRow = 1
+    let m = matrix.length, n = matrix[0].length
+
     for(let i=0; i<m; i++) {
         for(let j=0; j<n; j++) {
             if(matrix[i][j] === 0) {
-                set.add(`r${i}`)
-                set.add(`c${j}`)
+                matrix[0][j] = 0
+                if(i === 0) {
+                    firstRow=0
+                } else {
+                    matrix[i][0] = 0
+                }
+
             }
         }
     }
-    // console.log(set)
-    for(let i=0; i<m; i++) {
-        const key = `r${i}`
-        if(set.has(key)) {
+
+    for(let i=1; i<m; i++) {
+        if(matrix[i][0] === 0) {
             for(let j=0; j<n; j++) {
                 matrix[i][j] = 0
             }
         }
     }
     for(let j=0; j<n; j++) {
-        const key = `c${j}`
-        if(set.has(key)) {
+        if(matrix[0][j] === 0) {
             for(let i=0; i<m; i++) {
                 matrix[i][j] = 0
             }
         }
     }
-
+    if(!firstRow) {
+        for(let i=0; i<n; i++) {
+            matrix[0][i] = 0
+        }
+    }
 };
