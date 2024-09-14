263239964fd4bb151b83b4476c3739e0507c87f1
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-14 03:17:17 -0400

    Time: 162 ms (15.06%), Space: 59.8 MB (5.42%) - LeetHub

--- 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.js
+++ 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.js
@@ -2,27 +2,29 @@
  * @param {number[]} ages
  * @return {number}
  */
-function isValid(age1, age2) {
-    if(age2 <= 0.5*age1+7) return false
-    if(age2 > age1) return false
-    return true
-}
-var numFriendRequests = function(ages) {
-    const map = new Map()
-    for(const age of ages) {
-        const f = map.get(age) || 0
-        map.set(age, f+1)
-    }
-    let res = 0
-    for(const [age1, f1] of map) {
-        for(const [age2, f2] of map) {
-            if(isValid(age1, age2)) {
-                res += f1*f2
-                if(age1 === age2) {
-                    res -= f1
-                }
-            }
+
+function upper_bound(start, end, ages, target) {
+    let l = start, r = end
+    while(l <= r) {
+        const m = l + parseInt((r-l)/2)
+        if(ages[m] <= target) {
+            l = m+1
+        } else {
+            r = m-1
         }
     }
-    return res
+    return l
+}
+var numFriendRequests = function(ages) {
+    // ages.push(16)
+    let count = 0
+    ages.sort((a,b) => a-b)
+    for(let i=0; i<ages.length; i++) {
+        // if(ages[i] === ages[i+1]) continue
+        const x = upper_bound(0, ages.length-1, ages, 0.5*ages[i]+7)
+        const y = upper_bound(0, ages.length-1, ages, ages[i])
+        // console.log("x: ", x, " : y: ", y)
+        count += Math.max(y-x-1,0)
+    }
+    return count
 };
