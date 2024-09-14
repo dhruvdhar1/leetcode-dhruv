061f0cfc9645c9c11477559fda271b85394f1831
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-14 15:32:05 -0400

    Time: 74 ms (29.3%), Space: 53 MB (39.76%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+function backtrack(candidates, target, ind, arr, res) {
+    if(target === 0) {
+        res.push([...arr])
+        return
+    }
+    if(target < 0) {
+        return
+    }
+
+    for(let i=ind; i<candidates.length; i++) {
+        if(i > ind && candidates[i] === candidates[i-1]) continue
+        // if(candidates[i] > target) break
+        arr.push(candidates[i])
+        backtrack(candidates, target-candidates[i], i+1, arr, res)
+        arr.pop()
+    }
+}
+
+var combinationSum2 = function(candidates, target) {
+    const res = []
+    candidates.sort((a,b) => a-b)
+    backtrack(candidates, target, 0, [], res)
+    console.log(res)
+    return res
+};
