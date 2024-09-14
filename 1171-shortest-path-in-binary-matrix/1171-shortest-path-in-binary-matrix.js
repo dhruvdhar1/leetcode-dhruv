cdddf0630bbd431d0619f210753c16fc874c7284
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-14 12:22:11 -0400

    Time: 89 ms (92.33%), Space: 57.7 MB (73%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.js
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.js
@@ -6,32 +6,35 @@ var shortestPathBinaryMatrix = function(grid) {
     let m = grid.length
     let n = grid[0].length
     const q = new Queue([[0,0,1]])
+    const dirs = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
     let reached = false
     while(q.size() > 0) {
         const len = q.size()
-        // for(let i=0; i<len; i++) {
-            const pop = q.dequeue()
-            const x = pop[0]
-            const y = pop[1]
-            const length = pop[2]
-            if(x < 0 || y < 0 || x >= m || y >= n || grid[x][y] === 1) {
-                continue
-            }
-            if(x === m-1 && y === n-1) {
-                reached = true
-                return length
-            }
-            grid[x][y] = 1
-
-            q.enqueue([x-1, y, length+1])
-            q.enqueue([x-1, y+1, length+1])
-            q.enqueue([x+1, y, length+1])
-            q.enqueue([x+1, y+1, length+1])
-            q.enqueue([x-1, y-1, length+1])
-            q.enqueue([x, y-1, length+1])
-            q.enqueue([x, y+1, length+1])
-            q.enqueue([x+1, y-1, length+1])
-        // }
+        const pop = q.dequeue()
+        const x = pop[0]
+        const y = pop[1]
+        const length = pop[2]
+        if(x < 0 || y < 0 || x >= m || y >= n || grid[x][y] === 1) {
+            continue
+        }
+        if(x === m-1 && y === n-1) {
+            reached = true
+            return length
+        }
+        grid[x][y] = 1
+        for(const dir of dirs) {
+            const dx = dir[0]
+            const dy = dir[1]
+            q.enqueue([x+dx, y+dy, length+1])
+        }
+        // q.enqueue([x-1, y, length+1])
+        // q.enqueue([x-1, y+1, length+1])
+        // q.enqueue([x+1, y, length+1])
+        // q.enqueue([x+1, y+1, length+1])
+        // q.enqueue([x-1, y-1, length+1])
+        // q.enqueue([x, y-1, length+1])
+        // q.enqueue([x, y+1, length+1])
+        // q.enqueue([x+1, y-1, length+1])
     }
     return -1
 };
