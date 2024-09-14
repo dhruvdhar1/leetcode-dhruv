7c9bbf845cf7f65feac3ffd08beadc5e13567da3
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-14 10:06:42 -0400

    Time: 222 ms (31.34%), Space: 82.1 MB (52.98%) - LeetHub

--- 1731-even-odd-tree/1731-even-odd-tree.js
+++ 1731-even-odd-tree/1731-even-odd-tree.js
@@ -7,33 +7,50 @@
  * }
  */
 /**
- * @param {TreeNode} root
- * @return {boolean}
- */
+                1
+              /   \
+             10     4
+           3        7  9
+         12  8     6     2    
 
+         [[1], [10,4], [3, 7, 9], [12, 8, 6, 2]]
+  */
 var isEvenOddTree = function(root) {
-    let q = [root] //dequeue
+    const q = new Queue([root])
     let level = 0
 
-    while(q.length > 0) {
-        const aux = []
-        let prev = null
-        while(q.length > 0) {
-            const pop = q.shift()
-            
-            if(level%2 === 0 && ((prev && prev >= pop.val) || pop.val%2 === 0)) return false
-            if(level%2 !== 0 && ((prev && prev <= pop.val) || pop.val%2 !== 0)) return false
-            prev = pop.val
+    while(q.size() > 0) {
+        const len = q.size()
+        const levelEl = []
+        if(level%2 === 0) {
+            levelEl.push(-1)
+        } else {
+            levelEl.push(10000000)
+        }
+        for(let i=0; i<len; i++) {
+            const pop = q.dequeue()
+            //todo: check increasing/dec
+            if(level%2 === 0 && (pop.val%2 === 0 || pop.val <= levelEl[levelEl.length-1])) {
+                // console.log("here...")
+                return false
+            }
+            if(level%2 !== 0 && (pop.val%2 !== 0 || pop.val >= levelEl[levelEl.length-1])) {
+                // console.log("odd: ", pop.val, levelEl, level, pop.val >= levelEl[levelEl.length-1], pop.value%2 !== 0)
+                return false
+            }
+            levelEl.push(pop.val)
             if(pop.left) {
-                aux.push(pop.left)
+                q.push(pop.left)
             }
             if(pop.right) {
-                aux.push(pop.right)
+                q.push(pop.right)
             }
         }
-        q = aux
         level++
     }
     return true
-
 };
+
+//level=2
+// q: [,7]
+//levelEl: [-1,3]
