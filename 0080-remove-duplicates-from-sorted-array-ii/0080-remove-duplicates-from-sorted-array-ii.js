a5cb074a0bbeb74c2be745540b9ad6924d074232
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-14 02:33:38 -0400

    Time: 62 ms (52.94%), Space: 50.9 MB (95.29%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -3,14 +3,13 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    if(nums.length <= 2) return nums.length
-
+    if(nums.length < 2) return nums.length
     let l=2, r=2
     while(r < nums.length) {
-        if(nums[l-2] !== nums[r]) {
+        if(nums[r] !== nums[l-2]) {
             nums[l] = nums[r]
             l++
-        }
+        } 
         r++
     }
     return l
