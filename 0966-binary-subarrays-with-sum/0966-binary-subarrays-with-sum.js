853bfecda83df72a865366a4671caa9bc5f75d07
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-13 20:45:26 -0400

    Time: 52 ms (94.15%), Space: 50.9 MB (87.77%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.js
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.js
@@ -3,16 +3,20 @@
  * @param {number} goal
  * @return {number}
  */
-var numSubarraysWithSum = function(nums, goal) {
-    const map = new Map([[0,1]])
-    let sum = 0, count = 0
-    for(const num of nums) {
-        sum += num
-        const diff = sum - goal
-        if(map.has(diff)) {
-            count += map.get(diff)
+function helper(x, nums, goal) {
+    if(x < 0) return 0
+
+    let l=0, res=0, sum=0
+    for(let r=0; r<nums.length; r++) {
+        sum += nums[r]
+        while(sum > x) {
+            sum -= nums[l]
+            l++
         }
-        map.set(sum, (map.get(sum) || 0) + 1)
+        res += (r-l+1)
     }
-    return count
+    return res
+}
+var numSubarraysWithSum = function(nums, goal) {
+    return helper(goal, nums, goal) - helper(goal-1, nums, goal)
 };
