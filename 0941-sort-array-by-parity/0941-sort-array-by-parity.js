cf48ee4bfc660ba499de46035ea339e646b4cc7f
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-15 14:51:01 -0400

    Time: 82 ms (8.82%), Space: 52.2 MB (42.19%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.js
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.js
@@ -3,11 +3,9 @@
  * @return {number[]}
  */
 var sortArrayByParity = function(nums) {
-
     let l=0, r=nums.length-1
     while(l < r) {
         if(nums[l] % 2 != 0 && nums[r] % 2 === 0) {
-            //swap
             const temp = nums[l]
             nums[l] = nums[r]
             nums[r] = temp
