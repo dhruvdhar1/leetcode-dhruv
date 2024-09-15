1aeb5b9e4fb3e585f64e0ad122f8d399249d1de2
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-15 12:06:13 -0400

    Time: 89 ms (10.83%), Space: 52.6 MB (45.28%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+function canEat(piles, k, h) {
+    let hours = 0
+    for(let j=0; j<piles.length; j++) {
+        hours += Math.ceil(piles[j]/k)
+    }
+    if(hours <= h) {
+        return true
+    }
+    return false
+}
+var minEatingSpeed = function(piles, h) {
+    piles.sort((a,b) => a-b)
+    const maxK = piles[piles.length-1]
+    let l = 1, r = maxK
+    let ans = -1
+    while(l <= r) {
+        const mid = Math.floor(l+(r-l)/2)
+        if(canEat(piles, mid, h)) {
+            r = mid-1
+            ans = mid
+        } else {
+            l = mid+1
+        }
+    }
+    return ans
+};
