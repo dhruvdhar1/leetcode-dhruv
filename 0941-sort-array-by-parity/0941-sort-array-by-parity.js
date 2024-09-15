6aeb79fa2a9d7679a3a7165716af59a65e2d7f35
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-15 14:45:37 -0400

    Time: 78 ms (12.85%), Space: 53.4 MB (6.52%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    const even = []
+    const odd = []
+    for(const num of nums) {
+        if(num%2 === 0) {
+            even.push(num)
+        } else {
+            odd.push(num)
+        }
+    }
+    return [...even, ...odd]
+};
