8068a22dbf0e659299413e9c07c07b196b88edfc
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-14 21:00:00 -0400

    Time: 88 ms (6.85%), Space: 57.3 MB (6.16%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {boolean}
+ */
+var validTree = function(n, edges) {
+    const visited = new Set()
+    visited.add(0)
+    const adj = new Map()
+    for(const edge of edges) {
+        const src = edge[0]
+        const dest = edge[1]
+        if(!adj.has(src)) {
+            adj.set(src, [])
+        }
+        if(!adj.has(dest)) {
+            adj.set(dest, [])
+        }
+        adj.get(src).push(dest)
+        adj.get(dest).push(src)
+    }
+    console.log(adj)
+    const q = new Queue([[0,-1]])
+    while(q.size() > 0) {
+        const pop = q.dequeue()
+        // console.log("ppp: ", pop)
+        const node = pop[0]
+        const parent = pop[1]
+
+        const neigh = adj.get(node) || []
+        // console.log(node, " : ", neigh)
+        for(const n of neigh) {
+            if(!visited.has(n)) {
+                visited.add(n)
+                q.enqueue([n, node])
+            } else {
+                if(parent != n) {
+                    return false
+                }
+            }
+        }
+        visited
+    }
+    // return true
+    return visited.size === n
+};
