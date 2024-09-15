33a365a00aacd88fe054d10beb73b1be7a415434
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-15 14:44:10 -0400

    Time: 71 ms (89.92%), Space: 60.2 MB (79.85%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.js
+++ 0932-monotonic-array/0932-monotonic-array.js
@@ -2,18 +2,7 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-function isIncreasing(nums) {
-    for(let i=1; i<nums.length; i++) {
-        if(nums[i-1] > nums[i]) return false
-    }
-    return true
-}
-function isDecreasing(nums) {
-    for(let i=1; i<nums.length; i++) {
-        if(nums[i-1] < nums[i]) return false
-    }
-    return true
-}
+
 var isMonotonic = function(nums) {
     let increase = true
     let decrease = true
