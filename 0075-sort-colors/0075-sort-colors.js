a74639a982234d7b9a5facb40c668c83d514d518
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-16 02:09:24 -0400

    Time: 53 ms (57.09%), Space: 49.1 MB (50.69%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -18,13 +18,15 @@ var sortColors = function(nums) {
             nums[l] = nums[m]
             nums[m] = temp
             l++
+            m++
         } else if(nums[m] === 2) {
             const temp = nums[r]
             nums[r] = nums[m]
             nums[m] = temp
             r--
-            m--
+            // m--
+        } else {
+            m++
         }
-        m++
     }
 };
