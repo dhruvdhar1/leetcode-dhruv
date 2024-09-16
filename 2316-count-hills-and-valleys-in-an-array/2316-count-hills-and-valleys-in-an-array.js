1bac86f0651aac34cb887ec913734a743aadde98
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-16 11:59:19 -0400

    Time: 99 ms (5.66%), Space: 56.9 MB (5.66%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.js
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.js
@@ -1,23 +1,22 @@
 /**
-              l     r 
-    [2  4  1  1  6  5]
-                 i
-
-
-    count = 3
+ * @param {number[]} nums
+ * @return {number}
  */
 var countHillValley = function(nums) {
     let count = 0
-    for(let i=1; i<nums.length-1; i++) {
+    let i = 1
+    while(i < nums.length-1) {
         let l = i-1, r = i+1
-        if(nums[i] === nums[i-1]) continue
-
+        if(nums[i-1] === nums[i]) {
+            i++
+            continue
+        }
         while(l > 0 && nums[l] == nums[i]) l--
         while(r < nums.length-1 && nums[r] == nums[i]) r++
-
-        if(nums[l] < nums[i] && nums[i] > nums[r]) count++
-        else if(nums[l] > nums[i] && nums[i] < nums[r]) count++
+        console.log(l, i, r)
+        if(nums[l] < nums[i] && nums[r] < nums[i]) count++
+        if(nums[l] > nums[i] && nums[r] > nums[i]) count++
+        i++
     }
-
     return count
 };
