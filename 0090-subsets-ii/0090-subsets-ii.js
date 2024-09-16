a8f39bf3d5f8aaf47a07e9355b98094134ad8d6c
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-16 02:37:07 -0400

    Time: 65 ms (77.58%), Space: 52.8 MB (98.05%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.js
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -4,29 +4,22 @@
  */
 function backtrack(nums, ind, curr, res) {
     if(ind >= nums.length) {
-        res.push(curr)
+        res.push([...curr])
         return
     }
-    backtrack(nums, ind+1, [...curr, nums[ind]], res)
+    curr.push(nums[ind])
+    backtrack(nums, ind+1, curr, res)
+    curr.pop()
+
     let k = 0
     while(nums[ind+k] === nums[ind]) {
         k++
     }
-    backtrack(nums, ind+k, [...curr], res)
+    backtrack(nums, ind+k, curr, res)
 }
 var subsetsWithDup = function(nums) {
     const set = []
     nums.sort((a,b) => a-b)
     backtrack(nums, 0, [], set)
-    const res = []
-    // console.log(set)
-    // for(const str of set) {
-    //     if(str === '') {
-    //         res.push([])
-    //     } else {
-    //         const arr = str.split(':')
-    //         res.push(arr)
-    //     }
-    // }
     return set
 };
