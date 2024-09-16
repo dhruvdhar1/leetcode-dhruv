9bdc6b2dea38e29339bd0b0b4c8755de38ac0e52
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-15 23:59:49 -0400

    Time: 323 ms (25.42%), Space: 93.8 MB (23.73%) - LeetHub

--- 1539-diagonal-traverse-ii/1539-diagonal-traverse-ii.js
+++ 1539-diagonal-traverse-ii/1539-diagonal-traverse-ii.js
@@ -3,11 +3,9 @@
  * @return {number[]}
  */
 var findDiagonalOrder = function(nums) {
-    let maxCols = -1
     const res = []
     const map = new Map()
     for(let i=0; i<nums.length; i++) {
-        maxCols = Math.max(maxCols, nums[i].length)
         for(j=0; j<nums[i].length; j++) {
             if(!map.has(i+j)) {
                 map.set(i+j, [])
@@ -15,10 +13,7 @@ var findDiagonalOrder = function(nums) {
             map.get(i+j).push(nums[i][j])
         }
     }
-    // console.log(map)
-    // const range = nums.length*maxCols
     for(const [key, list] of map) {
-        // const list = map.get(i) || []
         while(list.length > 0) {
             res.push(list.pop())
         }
