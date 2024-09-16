d5b694817a87669995afc9835ba4762cad982419
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-16 02:08:08 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -1,23 +1,29 @@
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
+
+    {
+    }
+
+    [0,1,2]
+       l            
+       r
+       m
  */
 var sortColors = function(nums) {
-    let l = 0, r = nums.length-1
-    let i = 0
-    while(i <= r) {
-        if(nums[i] === 0) {
-            const temp = nums[i]
-            nums[i] = nums[l]
-            nums[l] = temp
+    let l = 0, m = 0, r = nums.length-1
+    while(m<=r) {
+        if(nums[m] === 0) {
+            const temp = nums[l]
+            nums[l] = nums[m]
+            nums[m] = temp
             l++
-        } else if(nums[i] === 2) {
-            const temp = nums[i]
-            nums[i] = nums[r]
-            nums[r] = temp
+        } else if(nums[m] === 2) {
+            const temp = nums[r]
+            nums[r] = nums[m]
+            nums[m] = temp
             r--
-            i--
         }
-        i++
+        m++
     }
 };
