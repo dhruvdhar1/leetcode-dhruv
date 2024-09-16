d4b49bc22b0e488dd811605a2d72432345e1c956
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-15 20:33:30 -0400

    Time: 199 ms (34.78%), Space: 60.5 MB (89.57%) - LeetHub

---
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} position
+ * @param {number} m
+ * @return {number}
+ */
+function getBallCount(positions, m) {
+    let count = 1, pivot = 0
+    for(let i=0; i<positions.length; i++) {
+        if(positions[i] - positions[pivot] >= m) {
+            count++
+            pivot = i
+        }
+    }
+    return count
+}
+var maxDistance = function(position, m) {
+    position.sort((a, b) => a-b)
+    if(m === 2) {
+        return position[position.length-1] - position[0]
+    }
+    let l=0, r = position[position.length-1]
+    let ans = -1
+    while(l <= r) {
+        const mid = l + Math.floor((r-l)/2)
+        const ballCount = getBallCount(position, mid)
+        if(ballCount >= m) {
+            l = mid + 1
+            ans = mid
+        } else {
+            r = mid - 1
+        }
+    }
+    return ans
+};
